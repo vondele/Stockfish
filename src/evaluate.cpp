549aad414adc0b9ba6922e9c6428d793fd03892f
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2016-11-17 21:04:01 +0100

    Hanging parameter tweak.
    Bench: 5780822

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -195,7 +195,7 @@ namespace {
   const Score ThreatByHangingPawn = S(71, 61);
   const Score LooseEnemies        = S( 0, 25);
   const Score WeakQueen           = S(35,  0);
-  const Score Hanging             = S(48, 27);
+  const Score Hanging             = S(55, 24);
   const Score ThreatByPawnPush    = S(38, 22);
   const Score Unstoppable         = S( 0, 20);
   const Score PawnlessFlank       = S(20, 80);
