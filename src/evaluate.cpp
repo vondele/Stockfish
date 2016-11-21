ee60555e714f63b5085bfd2ba5f43ea915d4e0e8
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2016-11-21 21:22:00 +0100

    Tweak unstoppable param.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -197,7 +197,7 @@ namespace {
   const Score WeakQueen           = S(35,  0);
   const Score Hanging             = S(48, 27);
   const Score ThreatByPawnPush    = S(38, 22);
-  const Score Unstoppable         = S( 0, 20);
+  const Score Unstoppable         = S( 0, 30);
   const Score PawnlessFlank       = S(20, 80);
   const Score HinderPassedPawn    = S( 7,  0);
 
