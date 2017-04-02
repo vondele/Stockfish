4c8d75282d99cac79e11285dff670ada43e56347
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-04-02 08:37:55 +0200

    Redefine stat_bonus. Bench: 6001297

--- src/movepick.h
+++ src/movepick.h
@@ -42,12 +42,12 @@ struct HistoryStats {
     Square from = from_sq(m);
     Square to = to_sq(m);
 
-    const int denom = 324;
+    const int denom = 10313;
 
     assert(abs(int(v)) <= denom); // Needed for stability.
 
     table[c][from][to] -= table[c][from][to] * abs(int(v)) / denom;
-    table[c][from][to] += int(v) * 32;
+    table[c][from][to] += int(v);
   }
 
 private:
@@ -69,12 +69,12 @@ struct Stats {
   void update(Piece pc, Square to, Move m) { table[pc][to] = m; }
   void update(Piece pc, Square to, Value v) {
 
-    const int denom = 936;
+    const int denom = 30764;
 
     assert(abs(int(v)) <= denom); // Needed for stability.
 
     table[pc][to] -= table[pc][to] * abs(int(v)) / denom;
-    table[pc][to] += int(v) * 32;
+    table[pc][to] += int(v);
   }
 
 private:
