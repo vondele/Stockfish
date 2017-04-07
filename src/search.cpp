1cfdfa7280fb97900801001d6a6bdff9327e884f
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-04-07 08:44:18 +0200

    Adjust correction factor. Bench: 5855549

--- src/search.cpp
+++ src/search.cpp
@@ -981,7 +981,7 @@ moves_loop: // When in check search starts from here
                            + fmh[moved_piece][to_sq(move)]
                            + fm2[moved_piece][to_sq(move)]
                            + thisThread->history.get(~pos.side_to_move(), move)
-                           - 4000; // Correction factor
+                           - 3400; // Correction factor
 
               // Decrease/increase reduction by comparing opponent's stat score
               if (ss->history > VALUE_ZERO && (ss-1)->history < VALUE_ZERO)
