32a35393c8b5becd55f8547dc315724437db2fc4
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-05-04 17:14:07 +0200

    Take 3. Bench: 5572184

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -19,6 +19,7 @@
 */
 
 #include <cassert>
+#include <iostream>
 
 #include "movepick.h"
 #include "thread.h"
@@ -159,7 +160,7 @@ void MovePicker::score<QUIETS>() {
                + fmh[pos.moved_piece(m)][to_sq(m)]
                + fm2[pos.moved_piece(m)][to_sq(m)]
                + history.get(c, m);
-      m.value+=std::abs(m.value)*(8-distance(to_sq(m),to_sq((ss-2)->currentMove)))/8;
+      m.value -= 256 * distance(to_sq(m),to_sq((ss-2)->currentMove));
   }
 
 }
