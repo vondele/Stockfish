cc80616d10364cc9b0b132ebd868da4247d137b4
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-05-18 17:57:42 +0200

    fixup

--- src/thread.cpp
+++ src/thread.cpp
@@ -160,6 +160,7 @@ void ThreadPool::read_uci_options() {
 /// ThreadPool::nodes_searched() returns the number of nodes searched
 
 uint64_t ThreadPool::nodes_searched() const {
+
   uint64_t nodes = 0;
   for (Thread* th : *this)
       nodes += th->rootPos.nodes_searched();
@@ -170,6 +171,7 @@ uint64_t ThreadPool::nodes_searched() const {
 /// ThreadPool::tb_hits() returns the number of TB hits
 
 uint64_t ThreadPool::tb_hits() const {
+
   uint64_t hits = 0;
   for (Thread* th : *this)
       hits += th->tbHits;
