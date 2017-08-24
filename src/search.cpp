6269def51cd8a5a12378be3733a836164b59a9f3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-08-24 07:20:09 +0200

    Take 3. Bench: 5709272

--- src/search.cpp
+++ src/search.cpp
@@ -960,7 +960,7 @@ moves_loop: // When in check search starts from here
           &&  moveCount > 1
           && (!captureOrPromotion || moveCountPruning))
       {
-          int mch = std::max(1, moveCount - (ss-1)->moveCount/8 - (ss-2)->moveCount/16 - (ss-3)->moveCount/32);
+          int mch = std::max(1, moveCount - ((ss-1)->moveCount + 4 * (ss-2)->moveCount + (ss-3)->moveCount)/32);
           Depth r = reduction<PvNode>(improving, depth, mch);
 
           if (captureOrPromotion)
