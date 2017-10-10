4fa036c831d3eddce902ef0314645a5fb15ee0f0
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-10-10 15:01:23 +0200

    More checks in QS

--- src/types.h
+++ src/types.h
@@ -212,8 +212,8 @@ enum Depth : int {
   ONE_PLY = 1,
 
   DEPTH_ZERO          =  0 * ONE_PLY,
-  DEPTH_QS_CHECKS     =  0 * ONE_PLY,
-  DEPTH_QS_NO_CHECKS  = -1 * ONE_PLY,
+  DEPTH_QS_CHECKS     = -1 * ONE_PLY,
+  DEPTH_QS_NO_CHECKS  = -2 * ONE_PLY,
   DEPTH_QS_RECAPTURES = -5 * ONE_PLY,
 
   DEPTH_NONE = -6 * ONE_PLY,
