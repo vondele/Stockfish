cc53497fee0892cb79559fdadb07546b7c61ab91
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-11-10 07:12:18 +0100

    verify depth value 13. Bench: 5208435

--- src/search.cpp
+++ src/search.cpp
@@ -709,7 +709,7 @@ namespace {
             if (nullValue >= VALUE_MATE_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (depth < 11 * ONE_PLY && abs(beta) < VALUE_KNOWN_WIN)
+            if (depth < 13 * ONE_PLY && abs(beta) < VALUE_KNOWN_WIN)
                 return nullValue;
 
             // Do verification search at high depths
