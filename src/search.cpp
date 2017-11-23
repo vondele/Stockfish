8a42acf7b7ab061e1c798dbc2d1565a175081ff0
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-11-23 12:37:38 +0100

    Take 5. Bench: 5816359

--- src/search.cpp
+++ src/search.cpp
@@ -684,7 +684,8 @@ namespace {
     // Step 8. Null move search with verification search (is omitted in PV nodes)
     if (   !PvNode
         &&  eval >= beta
-        &&  pos.plies_from_null() > 4)
+        &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
+        &&  pos.plies_from_null() > 3)
     {
 
         assert(eval - beta >= 0);
