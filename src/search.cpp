553941d144c6838746770673fd24a21ff3a32963
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-11-28 17:59:48 +0100

    Take 2. Bench: 5046605

--- src/search.cpp
+++ src/search.cpp
@@ -690,8 +690,8 @@ namespace {
     // Step 8. Null move search with verification search (is omitted in PV nodes)
     if (   !PvNode
         &&  eval >= beta
-        &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
-        && (pos.plies_from_null() % 2 == 0 || pos.plies_from_null() > thisThread->drawIter)
+        &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 
+          ((pos.plies_from_null() % 2 == 0 || pos.plies_from_null() > thisThread->drawIter) ? 225 : 225 + 100 * thisThread->drawIter)
         &&  pos.non_pawn_material(pos.side_to_move()))
     {
 
