a9adab85f3a2926c3a79bd2d049ed924706c9477
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-12-12 18:53:05 +0100

    Take 6. Bench: 5194333

--- src/search.cpp
+++ src/search.cpp
@@ -531,7 +531,10 @@ namespace {
     {
         // Step 2. Check for aborted search and immediate draw
         if (Threads.stop.load(std::memory_order_relaxed) || pos.is_draw(ss->ply) || ss->ply >= MAX_PLY)
-           return ss->ply >= MAX_PLY && !inCheck ? evaluate(pos) : (ss->ply < 16 ? VALUE_DRAW : ((ss-1)->staticEval < 0 ? VALUE_DRAW + Value(1) : VALUE_DRAW + Value(-1)));
+        return ss->ply >= MAX_PLY && !inCheck ? evaluate(pos) :
+                  ((ss->ply < 16 || (ss-1)->staticEval == VALUE_NONE) ? VALUE_DRAW :
+                       ((ss-1)->staticEval < 0 ? std::min( Value(3), -(ss-1)->staticEval / 64) :
+                                                 std::max(-Value(3), -(ss-1)->staticEval / 64)));
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply+1), but if alpha is already bigger because
@@ -1140,7 +1143,10 @@ moves_loop: // When in check search starts from here
 
     // Check for an instant draw or if the maximum ply has been reached
     if (pos.is_draw(ss->ply) || ss->ply >= MAX_PLY)
-        return ss->ply >= MAX_PLY && !InCheck ? evaluate(pos) : (ss->ply < 16 ? VALUE_DRAW : ((ss-1)->staticEval < 0 ? VALUE_DRAW + Value(1) : VALUE_DRAW + Value(-1)));
+        return ss->ply >= MAX_PLY && !InCheck ? evaluate(pos) :
+                  ((ss->ply < 16 || (ss-1)->staticEval == VALUE_NONE) ? VALUE_DRAW :
+                       ((ss-1)->staticEval < 0 ? std::min( Value(3), -(ss-1)->staticEval / 64) :
+                                                 std::max(-Value(3), -(ss-1)->staticEval / 64)));
 
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
 
