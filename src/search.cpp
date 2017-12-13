703e3a678120c9a64ecb0b8f4817df4e9a3816af
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-12-13 17:39:35 +0100

    Take 2. Bench: 5437196

--- src/search.cpp
+++ src/search.cpp
@@ -659,7 +659,7 @@ namespace {
 
     // local exploration
     if (PvNode &&  depth > 8 * ONE_PLY)
-        search<PV>(pos, ss, -VALUE_INFINITE, VALUE_INFINITE, 5 * ONE_PLY, false, true);
+        search<NonPV>(pos, ss, -VALUE_INFINITE, VALUE_INFINITE, 5 * ONE_PLY, false, true);
 
     // Step 6. Razoring (skipped when in check)
     if (   !PvNode
