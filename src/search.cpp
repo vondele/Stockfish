7c3d598d2b89d5d29bf1aa38e194c21b9bc0b866
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-12-20 18:14:38 +0100

    Save draw at lower depth. Bench: 5759818

--- src/search.cpp
+++ src/search.cpp
@@ -1105,7 +1105,7 @@ moves_loop: // When in check search starts from here
         tte->save(posKey, value_to_tt(bestValue, ss->ply),
                   bestValue >= beta ? BOUND_LOWER :
                   PvNode && bestMove ? BOUND_EXACT : BOUND_UPPER,
-                  depth, bestMove, ss->staticEval, TT.generation());
+                  (bestValue == VALUE_DRAW) ? depth - 4 * ONE_PLY : depth, bestMove, ss->staticEval, TT.generation());
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
 
