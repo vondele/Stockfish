13215c56bbe54289c486ea4a25cc564a116679d5
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-12-22 19:05:39 +0100

    Another tweak. Bench: 5050544

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -795,11 +795,11 @@ namespace {
             // is almost a draw, in case of KBP vs KB, it is even more a draw.
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                return more_than_one(pos.pieces(PAWN)) ? ScaleFactor(31) : ScaleFactor(9);
+                return more_than_one(pos.pieces(PAWN)) ? ScaleFactor(33) : ScaleFactor(9);
 
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
-            return ScaleFactor(46);
+            return ScaleFactor(44 + 8 * bool(pe->passed_pawns(strongSide)));
         }
         // Endings where weaker side can place his king in front of the opponent's
         // pawns are drawish.
