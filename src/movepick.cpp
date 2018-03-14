4a79a7b2f118392957b85a6fd18e22f7d726452f
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-03-14 19:56:24 +0100

    Take 2. Bench: 5241958

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -116,8 +116,8 @@ void MovePicker::score() {
   for (auto& m : *this)
       if (Type == CAPTURES)
           m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
-                   + (to_sq(m) == recaptureSquare) * 64
-                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+                   + (2 * (to_sq(m) == recaptureSquare) + 16) *
+                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))] / 16;
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
