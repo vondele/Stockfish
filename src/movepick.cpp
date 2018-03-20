41eb949c66ccb054f9dd6b9057255b41c8db7171
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-03-20 21:56:45 +0100

    Take 4. Bench: 4989534

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -184,8 +184,8 @@ top:
           || refutations[1].move == refutations[2].move)
           --endMoves;
       score<QUIETS>();
-      refutations[1].value-=5500;
-      refutations[2].value-=12500;
+      refutations[1].value-=1000;
+      refutations[2].value-=11500;
       ++stage;
       /* fallthrough */
 
