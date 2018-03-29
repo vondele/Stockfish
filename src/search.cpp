1ab28d4445d5ed23e270c4c31f7a8d7b2864d517
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-03-29 20:00:50 +0200

    Bench: 6579490

--- src/search.cpp
+++ src/search.cpp
@@ -891,7 +891,7 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning
-              if (moveCountPruning)
+              if (moveCountPruning && false)
               {
                   skipQuiets = true;
                   continue;
