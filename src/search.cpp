995577d4eae0f0d10519eb1103b21d041e5799d3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-03-29 20:05:32 +0200

    Bench: 6790511

--- src/search.cpp
+++ src/search.cpp
@@ -917,7 +917,7 @@ moves_loop: // When in check, search starts from here
                   && !pos.see_ge(move, Value(-35 * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (    depth < 7 * ONE_PLY
+          else if (    depth < 7 * ONE_PLY && false
                    && !extension
                    && !pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY)))
                   continue;
