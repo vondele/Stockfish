86d03b0a4a5227feaeae4071921ad4eca9a0f6d5
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-04-02 10:46:23 +0200

    Slope. Bench: 5268600

--- src/search.cpp
+++ src/search.cpp
@@ -873,7 +873,7 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3 * ONE_PLY
           &&  pos.legal(move))
       {
-          Value rBeta = std::max(ttValue - 2 * depth / ONE_PLY, -VALUE_MATE);
+          Value rBeta = std::max(ttValue + 8 - 3 * depth / ONE_PLY, -VALUE_MATE);
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, rBeta - 1, rBeta, depth / 2, cutNode, true);
           ss->excludedMove = MOVE_NONE;
