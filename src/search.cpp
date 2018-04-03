5f3f63baa3254ebda818cdfffa5dbbc679e75d6d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-04-03 20:04:22 +0200

    Take 5. Bench: 5184206

--- src/search.cpp
+++ src/search.cpp
@@ -870,7 +870,7 @@ moves_loop: // When in check, search starts from here
           && !excludedMove // Recursive singular search is not allowed
           &&  ttValue != VALUE_NONE
           && (tte->bound() & BOUND_LOWER)
-          &&  tte->depth() >= (15 * depth - 18 * ONE_PLY) / 16
+          &&  tte->depth() >= (15 * depth - 14 * ONE_PLY) / 16
           &&  pos.legal(move))
       {
           Value rBeta = std::max(ttValue - 2 * depth / ONE_PLY, -VALUE_MATE);
