5cd67df9c6e2029e9692e4e8b101b493ce6daf26
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-04-04 21:58:08 +0200

    Take 1: 5173859

--- src/search.cpp
+++ src/search.cpp
@@ -914,6 +914,11 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
+              if (   lmrDepth < 2
+                  && (   (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+                      || (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold))
+                  continue;
+
               // Futility pruning: parent node (~2 Elo)
               if (   lmrDepth < 7
                   && !inCheck
