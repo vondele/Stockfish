7b708a9ba6598325f4b26cb045bbd9994fef0c35
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-04-09 07:49:39 +0200

    use depth 4. Bench: 4865325

--- src/search.cpp
+++ src/search.cpp
@@ -763,7 +763,7 @@ namespace {
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
-        &&  depth >= 5 * ONE_PLY
+        &&  depth >= 4 * ONE_PLY
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
         assert(is_ok((ss-1)->currentMove));
@@ -781,15 +781,13 @@ namespace {
                 ss->currentMove = move;
                 ss->contHistory = thisThread->contHistory[pos.moved_piece(move)][to_sq(move)].get();
 
-                assert(depth >= 5 * ONE_PLY);
-
                 pos.do_move(move, st);
 
                 // Perform a preliminary qsearch to verify that the move holds
                 value = -qsearch<NonPV>(pos, ss+1, -rbeta, -rbeta+1);
 
                 // If the qsearch held perform the regular search
-                if (value >= rbeta)
+                if (value >= rbeta && depth > 4)
                     value = -search<NonPV>(pos, ss+1, -rbeta, -rbeta+1, depth - 4 * ONE_PLY, !cutNode, false);
 
                 pos.undo_move(move);
