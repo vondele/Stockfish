e407fac819b70b63462c236e77f452885fa237e6
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-04-14 09:30:39 +0200

    Use captHist for prune. Bench: 4891965

--- src/search.cpp
+++ src/search.cpp
@@ -925,10 +925,18 @@ moves_loop: // When in check, search starts from here
                   && !pos.see_ge(move, Value(-35 * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (    depth < 7 * ONE_PLY // (~20 Elo)
+          else
+          {
+              if (   depth < 3 * ONE_PLY
+                  && captureOrPromotion
+                  && thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
+                  continue;
+
+              if (    depth < 7 * ONE_PLY // (~20 Elo)
                    && !extension
                    && !pos.see_ge(move, -Value(CapturePruneMargin[depth / ONE_PLY])))
                   continue;
+          }
       }
 
       // Speculative prefetch as early as possible
