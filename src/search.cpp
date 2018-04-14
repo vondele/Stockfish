08a9a07e126939785278db53ef0cb55eb6419713
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-04-14 09:57:16 +0200

    Take 2. Bench: 4480269

--- src/search.cpp
+++ src/search.cpp
@@ -929,6 +929,7 @@ moves_loop: // When in check, search starts from here
           {
               if (   depth < 3 * ONE_PLY
                   && captureOrPromotion
+                  && !extension
                   && thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
