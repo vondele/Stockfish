f7441ba6be08ff89c5eae262b8ef7fc84aefd247
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-05-07 18:57:24 +0200

    Remove cutNode. Take 4. Bench: 4486538

--- src/search.cpp
+++ src/search.cpp
@@ -996,7 +996,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Increase reduction for cut nodes (~5 Elo)
-              if (!PvNode && (ss-1)->moveCount>4)
+              if (!PvNode && (ss-1)->moveCount>5)
                   r += 2 * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
