9c8331aaa192ae845f0964e6d78fb74e664cd4ba
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-05-09 07:03:59 +0200

    increase margin a bit. Bench: 5119723

--- src/search.cpp
+++ src/search.cpp
@@ -894,7 +894,7 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3 * ONE_PLY
           &&  pos.legal(move))
       {
-          Value rBeta = std::max(ttValue - 2 * depth / ONE_PLY, -VALUE_MATE);
+          Value rBeta = std::max(ttValue - 2 * depth / ONE_PLY + 4, -VALUE_MATE);
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, rBeta - 1, rBeta, depth / 2, cutNode);
           ss->excludedMove = MOVE_NONE;
