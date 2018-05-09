ea3062ddcf54985dc022186cfe885f76a54ee677
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-05-09 20:31:25 +0200

    Pruning at low depth.. extension vs check. Bench: 5040704

--- src/search.cpp
+++ src/search.cpp
@@ -916,7 +916,7 @@ moves_loop: // When in check, search starts from here
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           if (   !captureOrPromotion
-              && !givesCheck
+              && !extension
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning (~30 Elo)
