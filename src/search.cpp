7a4dd5e30ab37d79c9bd1db6eb51db04c286a738
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-05-18 21:45:30 +0200

    Stat for probcut. Take 1. Bench: 4929244

--- src/search.cpp
+++ src/search.cpp
@@ -796,7 +796,7 @@ namespace {
         &&  depth >= 5 * ONE_PLY
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
-        Value rbeta = std::min(beta + 216 - 48 * improving, VALUE_INFINITE);
+        Value rbeta = std::min(beta + 216 - 48 * improving + (ss-1)->statScore / 2048, VALUE_INFINITE);
         MovePicker mp(pos, ttMove, rbeta - ss->staticEval, &thisThread->captureHistory);
         int probCutCount = 0;
 
