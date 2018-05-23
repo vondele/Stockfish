ce2393e21dbd0df68ecafbebbef65cc5630d9a4a
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-05-23 21:34:35 +0200

    depth 3 prune. Bench: 4813842

--- src/search.cpp
+++ src/search.cpp
@@ -946,6 +946,11 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
+              if (   lmrDepth == 3
+                  && (*contHist[0])[movedPiece][to_sq(move)] < -4000
+                  && (*contHist[1])[movedPiece][to_sq(move)] < -4000)
+                  continue;
+
               // Futility pruning: parent node (~2 Elo)
               if (   lmrDepth < 7
                   && !inCheck
