5641e819e1266d532599a892987218fafd22f69b
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-06-06 23:30:42 +0200

    Take 8, symmetrize take 5, Bench: 4441320

--- src/search.cpp
+++ src/search.cpp
@@ -486,7 +486,7 @@ void Thread::search() {
               double bestMoveInstability = 1.0 + mainThread->bestMoveChanges;
               bestMoveInstability *= std::pow(mainThread->previousTimeReduction, 0.528) / timeReduction;
 
-              if (bestValue >= PawnValueEg / 2)
+              if (std::abs(bestValue) >= PawnValueEg * 3 / 4)
                   bestMoveInstability *= 1.1;
 
               // Stop the search if we have only one legal move, or if available time elapsed
