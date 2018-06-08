ee52d5b61b9686fb76df718e2a1f09f09ef6cc7c
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-06-08 07:13:31 +0200

    Take 4. Param 1.8/2.25. Bench: 4441320

--- src/search.cpp
+++ src/search.cpp
@@ -476,7 +476,7 @@ void Thread::search() {
                      timeReduction *= 1.25;
 
               // Use part of the gained time from a previous stable move for the current move
-              double bestMoveInstability = 1.0 + 2.0/std::pow(1.8, (completedDepth - lastBestMoveDepth) / ONE_PLY);
+              double bestMoveInstability = 1.0 + 1.83/std::pow(2.25, (completedDepth - lastBestMoveDepth) / ONE_PLY);
               bestMoveInstability *= std::pow(mainThread->previousTimeReduction, 0.528) / timeReduction;
 
               // Stop the search if we have only one legal move, or if available time elapsed
