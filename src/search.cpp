f0cccc7ea6afd08535d8a80795ab89394a1d204f
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-08-23 08:27:24 +0200

    No depth condition. Bench: 4463230

--- src/search.cpp
+++ src/search.cpp
@@ -631,7 +631,7 @@ namespace {
 
     // At non-PV nodes we check for an early TT cutoff
     if (  !PvNode
-        && !(depth > 2 && thisThread->pvDraw)
+        && !thisThread->pvDraw
         && ttHit
         && tte->depth() >= depth
         && ttValue != VALUE_NONE // Possible in case of TT access race
