2c3ca08bb93941f1e5ff8f8eff3d02ecb9881a23
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-05 07:54:10 +0200

    No improving if seemingly Draw. Bench: 4349728

--- src/search.cpp
+++ src/search.cpp
@@ -743,8 +743,8 @@ namespace {
         && eval <= alpha - RazorMargin)
         return qsearch<NT>(pos, ss, alpha, beta);
 
-    improving =   ss->staticEval >= (ss-2)->staticEval
-               || (ss-2)->staticEval == VALUE_NONE;
+    improving =    (ss->staticEval >= (ss-2)->staticEval || (ss-2)->staticEval == VALUE_NONE)
+                && !(ttHit && ttValue == VALUE_DRAW);
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !rootNode
