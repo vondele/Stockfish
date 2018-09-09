83b36b590cb51861aeadf45836709c1bd8a77cc3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-09 07:59:55 +0200

    more reduction if <14 moves. Bench: 4369778

--- src/search.cpp
+++ src/search.cpp
@@ -1004,7 +1004,7 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
-          if (countedMoves < 18)
+          if (countedMoves < 14)
               r += ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
