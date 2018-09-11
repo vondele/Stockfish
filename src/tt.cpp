6c41518c46238c2f9bb3855d71c7b80941663d9d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-11 11:13:45 +0200

    adjust replacement for draw. Take 1. Bench: 4329596

--- src/tt.cpp
+++ src/tt.cpp
@@ -40,7 +40,7 @@ void TTEntry::save(Key k, Value v, Bound b, Depth d, Move m, Value ev) {
 
   // Overwrite less valuable entries
   if (  (k >> 48) != key16
-      || d / ONE_PLY > depth8 - 4
+      || d / ONE_PLY > depth8 - 4 - 4 * (eval16 == VALUE_DRAW)
       || b == BOUND_EXACT)
   {
       key16     = (uint16_t)(k >> 48);
