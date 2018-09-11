c8f6eeda7d04a6fdc76b0d0602f48df2684b0325
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-11 23:25:41 +0200

    Take 3. Bench 4398139

--- src/search.cpp
+++ src/search.cpp
@@ -627,8 +627,7 @@ namespace {
     // At non-PV nodes we check for an early TT cutoff
     if (  !PvNode
         && ttHit
-        && tte->depth() >= depth
-        && !(thisThread->rootMoves[thisThread->pvIdx].previousScore == VALUE_DRAW)
+        && tte->depth() >= depth + 4 * (thisThread->rootMoves[thisThread->pvIdx].previousScore == VALUE_DRAW && ttValue == VALUE_DRAW)
         && ttValue != VALUE_NONE // Possible in case of TT access race
         && (ttValue >= beta ? (tte->bound() & BOUND_LOWER)
                             : (tte->bound() & BOUND_UPPER)))
