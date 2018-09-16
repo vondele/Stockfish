06fff17b827fc7b8eafecd27054b153608e65f25
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-16 17:03:21 +0200

    Manoeuvring moves. Take 4. Bench: 4042442

--- src/search.cpp
+++ src/search.cpp
@@ -1006,7 +1006,7 @@ moves_loop: // When in check, search starts from here
               r -= ONE_PLY;
 
           if (to_sq((ss-2)->currentMove) == from_sq(ss->currentMove))
-              r -= ONE_PLY;
+              r += ONE_PLY;
 
           if (!captureOrPromotion)
           {
