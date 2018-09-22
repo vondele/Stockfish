d295e6fd7857605ebd8e9c4514711eea78975c06
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-22 22:38:38 +0200

    Take 3. Bench: 4419881

--- src/search.cpp
+++ src/search.cpp
@@ -1006,7 +1006,7 @@ moves_loop: // When in check, search starts from here
           // extend jumping in an OCB game 
           if (   !wasOCB
               && pos.opposite_bishops())
-              r -= ONE_PLY;
+              r += ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
