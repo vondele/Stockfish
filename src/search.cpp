49690971ad57ca7f63ebee3306e90fa7fbf7265b
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-23 19:39:43 +0200

    Take 5. Bench: 4200447

--- src/search.cpp
+++ src/search.cpp
@@ -748,8 +748,8 @@ namespace {
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !rootNode
-        &&  depth < 7 * ONE_PLY - 2 * (ttMove == MOVE_NONE)
-        &&  eval - futility_margin(depth, improving && (ttMove != MOVE_NONE)) >= beta
+        &&  depth < 7 * ONE_PLY
+        &&  eval - futility_margin(depth - (eval == ttValue && 2 * tte->depth() > depth ? ONE_PLY : DEPTH_ZERO), improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
 
