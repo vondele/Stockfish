9597d9639d93556bf9363033b05e93c2f54d4824
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-23 17:11:22 +0200

    Futility and ttMoves. Take 3. Bench: 4843094

--- src/search.cpp
+++ src/search.cpp
@@ -748,8 +748,8 @@ namespace {
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !rootNode
-        &&  depth < 7 * ONE_PLY - (ttMove == MOVE_NONE)
-        &&  eval - futility_margin(depth, improving) >= beta
+        &&  depth < 7 * ONE_PLY
+        &&  eval - futility_margin(depth, improving && (ttMove != MOVE_NONE)) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
 
