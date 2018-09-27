2d62040772dee37b6ff984e45de62e67c826e5c8
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-27 07:48:14 +0200

    Moves Pawn. Take 1. Bench: 4633034

--- src/search.cpp
+++ src/search.cpp
@@ -940,7 +940,7 @@ moves_loop: // When in check, search starts from here
       {
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
+              && (type_of(movedPiece) != PAWN || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
