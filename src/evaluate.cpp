59dc3b320ae16e3cd5a9394d5e084f2a05506d8a
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-28 08:37:49 +0200

    Take 3. Bench: 4398836

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -791,7 +791,7 @@ namespace {
             && pos.non_pawn_material(BLACK) == BishopValueMg)
             sf = 8 + 4 * pe->pawn_asymmetry();
         else
-            sf = std::min(26 + 2 * pos.count<PAWN>(strongSide) + 4 * pe->pawn_asymmetry() + !pos.opposite_bishops() * 5 * pos.count<PAWN>(strongSide), sf);
+            sf = std::min(26 + 2 * pos.count<PAWN>(strongSide) + 4 * pe->pawn_asymmetry() + !pos.opposite_bishops() * 5 * pos.count<PAWN>(strongSide), sf - 16 * (pe->pawn_asymmetry()==0));
     }
 
     return ScaleFactor(sf);
