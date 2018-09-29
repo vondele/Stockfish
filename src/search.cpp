2e4d6c78eee76141f93aef469deb4a167b201675
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-09-29 21:13:51 +0200

    Take 5. Bench: 4059356

--- src/search.cpp
+++ src/search.cpp
@@ -327,7 +327,7 @@ void Thread::search() {
 
   int ct = int(Options["Contempt"]) * PawnValueEg / 100; // From centipawns
 
-  float complexityAdjustment=complexity(rootPos) > 10 ? 1.1 : 1.0;
+  float complexityAdjustment=complexity(rootPos) > 20 ? 1.1 : 1.0;
 
   // In analysis mode, adjust contempt in accordance with user preference
   if (Limits.infinite || Options["UCI_AnalyseMode"])
