ac1048f6c5bef7208db48d339a43a573bf063fa7
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-03 07:14:37 +0200

    Take 9. Bench: 4319025

--- src/search.cpp
+++ src/search.cpp
@@ -815,7 +815,7 @@ namespace {
         while (  (move = mp.next_move()) != MOVE_NONE
                && probCutCount++ < 6)
             if (   move != excludedMove
-                && thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] > 0
+                && thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] > 1000
                 && pos.legal(move))
             {
                 ss->currentMove = move;
