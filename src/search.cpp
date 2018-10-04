dbd4886e2d8e46ebc885e387226f981bd13fa16a
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-04 21:47:57 +0200

    Extensions for checking capt. Param 400.  Take 2. Bench: 4444017

--- src/search.cpp
+++ src/search.cpp
@@ -925,8 +925,7 @@ moves_loop: // When in check, search starts from here
               extension = ONE_PLY;
       }
       else if (    givesCheck // Check extension (~2 Elo)
-               && !moveCountPruning
-               &&  pos.see_ge(move, Value(captureOrPromotion ? -200 : 0)))
+               &&  pos.see_ge(move, Value(captureOrPromotion ? -400 : 0)))
           extension = ONE_PLY;
 
       // Calculate new depth for this move
