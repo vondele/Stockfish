758801db46bf60635edd8e35511c0059b2ff64be
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-04 07:25:14 +0200

    Take 4. Allow depth 4. Bench: 4498717

--- src/search.cpp
+++ src/search.cpp
@@ -994,7 +994,7 @@ moves_loop: // When in check, search starts from here
 
       // Step 16. Reduced depth search (LMR). If the move fails high it will be
       // re-searched at full depth.
-      if (    depth >= 2 * ONE_PLY
+      if (    depth >= 4 * ONE_PLY
           &&  moveCount > 1
           && (!captureOrPromotion || moveCountPruning))
       {
