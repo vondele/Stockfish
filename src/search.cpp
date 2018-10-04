78afcb74800af25dfa9b6e512611263e24cc746f
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-04 14:52:50 +0200

    make depth dependent. Take 1. Bench: 4340057

--- src/search.cpp
+++ src/search.cpp
@@ -1016,7 +1016,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += 2 * ONE_PLY;
+                  r += depth <= 5 * ONE_PLY ? ONE_PLY : 2 * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
