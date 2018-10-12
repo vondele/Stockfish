09591cccc164c566e9d419d8c1732880df5c88ac
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-12 15:04:07 +0200

    Tweak margin. Bench: 4239163

--- src/search.cpp
+++ src/search.cpp
@@ -1322,7 +1322,7 @@ moves_loop: // When in check, search starts from here
       {
           assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push
 
-          futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
+          futilityValue = futilityBase + PieceValue[MG][pos.piece_on(to_sq(move))];
 
           if (futilityValue <= alpha)
           {
