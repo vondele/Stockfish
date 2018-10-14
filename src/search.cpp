8ae5529dc6f38af64a23811d117ed9ba7409d571
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-14 17:46:18 +0200

    pvDist and mcp. Param 5. Take 2. Bench: 4238401

--- src/search.cpp
+++ src/search.cpp
@@ -899,7 +899,7 @@ moves_loop: // When in check, search starts from here
       givesCheck = gives_check(pos, move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving && ss->pvDist <= 5][depth / ONE_PLY];
+                        && moveCount >= FutilityMoveCounts[improving || ss->pvDist > 5][depth / ONE_PLY];
 
       // Step 13. Extensions (~70 Elo)
 
