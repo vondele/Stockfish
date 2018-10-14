f448cb7e78b95dec015ca99ed0ab75037816787a
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-14 11:12:26 +0200

    pvDist in NMP. Take 2. Bench: 4867835

--- src/search.cpp
+++ src/search.cpp
@@ -754,7 +754,7 @@ namespace {
         return eval;
 
     // Step 9. Null move search with verification search (~40 Elo)
-    if (   ss->pvDist > 1
+    if (   ss->pvDist > 2
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 23200
         &&  eval >= beta
