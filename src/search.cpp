409790a116938dc506d4da45a4bf56e2589496b3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-15 22:53:11 +0200

    Tweak alpha on fH. Take 4. Bench: 4629977

--- src/search.cpp
+++ src/search.cpp
@@ -442,7 +442,7 @@ void Thread::search() {
               }
               else if (bestValue >= beta) 
               {
-                  alpha = (5 * alpha - beta) / 4;
+                  alpha = (13 * alpha + 3 * beta) / 16;
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
               }
               else
