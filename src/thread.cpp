03a2a9e01dba51bf4daa61ed91aa6f9434350911
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-20 09:12:28 +0200

    Make delta depending on expected depth to completion. Bench: 5138009

--- src/thread.cpp
+++ src/thread.cpp
@@ -66,6 +66,7 @@ void Thread::clear() {
           h->fill(0);
 
   continuationHistory[NO_PIECE][0]->fill(Search::CounterMovePruneThreshold - 1);
+  previousCompletedDepth = DEPTH_ZERO;
 }
 
 /// Thread::start_searching() wakes up the thread that will start the search
