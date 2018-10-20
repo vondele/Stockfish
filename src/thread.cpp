fc99263a7d38c2599ce5c84897c8afe945db2e8b
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-20 15:13:15 +0200

    Take 3. Bench: 4382147

--- src/thread.cpp
+++ src/thread.cpp
@@ -66,7 +66,7 @@ void Thread::clear() {
           h->fill(0);
 
   continuationHistory[NO_PIECE][0]->fill(Search::CounterMovePruneThreshold - 1);
-  previousCompletedDepth = DEPTH_ZERO;
+  previousCompletedDepth = 13 * ONE_PLY;
 }
 
 /// Thread::start_searching() wakes up the thread that will start the search
