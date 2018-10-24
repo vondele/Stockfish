e49625d1f297ef5750bd5e167ef34ca7a78e0cdc
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-24 21:23:55 +0200

    Reductions and pawn moves. Take 2. Bench: 5132310

--- src/search.cpp
+++ src/search.cpp
@@ -1023,7 +1023,7 @@ moves_loop: // When in check, search starts from here
                   r += 2 * ONE_PLY;
 
               if (!pos.advanced_pawn_push(move) && type_of(movedPiece)==PAWN)
-                  r += ONE_PLY;
+                  r -= ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
