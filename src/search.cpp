9304125cd606888d5a728bd3299b5f4cd661eac9
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-28 22:18:39 +0100

    dyn. ct. Param 150. Bench: 3286319

--- src/search.cpp
+++ src/search.cpp
@@ -393,7 +393,7 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + 88 * previousScore / (abs(previousScore) + 100);
+              int dct = ct + 88 * previousScore / (abs(previousScore) + 150);
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
