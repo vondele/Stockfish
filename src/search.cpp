a4164d10e0b5db443ea57ad5ec5eca133424e24e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-29 13:30:57 +0100

    Single step fh. Bench: 3225569

--- src/search.cpp
+++ src/search.cpp
@@ -402,10 +402,10 @@ void Thread::search() {
           // Start with a small aspiration window and, in the case of a fail
           // high/low, re-search with a bigger window until we don't fail
           // high/low anymore.
-          int failedHighCnt = 0;
+          bool failedHigh = false;
           while (true)
           {
-        	  adjustedDepth = std::max(ONE_PLY, rootDepth - failedHighCnt * ONE_PLY);
+              adjustedDepth = std::max(ONE_PLY, rootDepth - failedHigh * 2 * ONE_PLY);
               bestValue = ::search<PV>(rootPos, ss, alpha, beta, adjustedDepth, false);
 
               // Bring the best move to the front. It is critical that sorting
@@ -439,7 +439,7 @@ void Thread::search() {
 
                   if (mainThread)
                   {
-                      failedHighCnt = 0;
+                      failedHigh = false;
                       failedLow = true;
                       Threads.stopOnPonderhit = false;
                   }
@@ -448,7 +448,7 @@ void Thread::search() {
               {
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
                   if (mainThread)
-                	  ++failedHighCnt;
+                      failedHigh = true;
               }
               else
                   break;
