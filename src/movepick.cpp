d062dbb3f772b0be851f12922206959e16320091
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-10-31 06:58:47 +0100

    Move castling moves up. Take 4. Bench: 3337211

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -118,7 +118,7 @@ void MovePicker::score() {
                    + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
-                   + (type_of(m) == CASTLING ? 18000 : 0);
+                   + (type_of(m) == CASTLING ? 5000 : 0);
 
       else // Type == EVASIONS
       {
