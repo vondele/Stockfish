187973c203d4b24f861cbfd738141f6b1f425bd2
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-11-03 15:04:29 +0100

    Take 2. Bench: 3846314

--- src/search.cpp
+++ src/search.cpp
@@ -942,10 +942,9 @@ moves_loop: // When in check, search starts from here
       else if (    givesCheck // Check extension (~2 Elo)
                &&  pos.see_ge(move))
           extension = ONE_PLY;
-
-      if (   pos.can_castle(us)
-          && type_of(movedPiece) == KING)
-          extension = ONE_PLY;
+      else if (   pos.can_castle(us)
+               && type_of(movedPiece) == KING)
+               extension = ONE_PLY;
 
       // Calculate new depth for this move
       newDepth = depth - ONE_PLY + extension;
