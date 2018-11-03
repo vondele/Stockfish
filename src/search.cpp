b170254512bbf8d1d238950561ff32e2e623967e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-11-03 15:23:58 +0100

    Take 4, Bench: 3112140

--- src/search.cpp
+++ src/search.cpp
@@ -880,6 +880,7 @@ moves_loop: // When in check, search starts from here
     skipQuiets = false;
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
     pvExact = PvNode && ttHit && tte->bound() == BOUND_EXACT;
+    bool canCastle = pos.can_castle(us);
 
     // Step 12. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -943,11 +944,6 @@ moves_loop: // When in check, search starts from here
                &&  pos.see_ge(move))
           extension = ONE_PLY;
       
-      if (   pos.can_castle(us)
-          && move == ttMove          
-          && type_of(movedPiece) == KING)
-          extension = ONE_PLY;
-
       // Calculate new depth for this move
       newDepth = depth - ONE_PLY + extension;
 
@@ -1033,6 +1029,10 @@ moves_loop: // When in check, search starts from here
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
                   r += 2 * ONE_PLY;
+
+              if (   canCastle
+                  && type_of(movedPiece) == KING)
+                  r += ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
