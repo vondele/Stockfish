9151a7e08d6166f9ad9ceb495059bbde59634f23
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-11-06 10:19:20 +0100

    Add depth condition. Bench: 3607243

--- src/search.cpp
+++ src/search.cpp
@@ -943,7 +943,8 @@ moves_loop: // When in check, search starts from here
                &&  pos.see_ge(move))
           extension = ONE_PLY;
 
-      if (   pos.can_castle(us)
+      if (   depth < 12 * ONE_PLY
+          && pos.can_castle(us)
           && type_of(movedPiece) == KING)
           extension = ONE_PLY;
 
