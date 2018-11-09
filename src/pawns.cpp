cd8fc0210fcdd8e723a1e121bd45272a77343feb
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-11-09 22:25:29 +0100

    Only evaluate Qside if no Kside castling. Bench: 3408881

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -252,8 +252,7 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   // If we can castle use the bonus after the castling if it is bigger
   if (pos.can_castle(Us | KING_SIDE))
       bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_G1)));
-
-  if (pos.can_castle(Us | QUEEN_SIDE))
+  else if (pos.can_castle(Us | QUEEN_SIDE))
       bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)));
 
   return make_score(bonus, -16 * minKingPawnDistance);
