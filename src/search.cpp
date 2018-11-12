ce5f52821b98ebd5aca972a76f93e7438758aa92
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-11-12 18:34:25 +0100

    Take 2. 10-20. Bench: 3727647

--- src/search.cpp
+++ src/search.cpp
@@ -949,8 +949,8 @@ moves_loop: // When in check, search starts from here
           && depth < 12 * ONE_PLY)
           extension = ONE_PLY;
 
-      if (   pos.rule50_count() > 20
-          && pos.rule50_count() < 30
+      if (   pos.rule50_count() > 10
+          && pos.rule50_count() < 20
           && (type_of(movedPiece) == PAWN || captureOrPromotion))
           extension = ONE_PLY;
 
