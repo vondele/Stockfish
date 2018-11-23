ff8bac41508b603479dead68ce1c14d67a1c9f46
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-11-23 23:06:42 +0100

    Stable PV time bonus. Param 1/4. Bench: 3679086

--- src/search.cpp
+++ src/search.cpp
@@ -518,7 +518,7 @@ void Thread::search() {
                   if (lastBestMoveDepth * i < completedDepth)
                      timeReduction *= 1.25;
  
-              if (int(equalPV) * ONE_PLY > rootDepth / 3)
+              if (int(equalPV) * ONE_PLY > rootDepth / 4)
                      timeReduction *= 1.5;
 
               // Use part of the gained time from a previous stable move for the current move
