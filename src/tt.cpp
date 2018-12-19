f19970c706d684c4f31cc3329c6e9a18a3176835
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-12-19 17:49:03 +0100

    Exp. 2. Bench: 3048598

--- src/tt.cpp
+++ src/tt.cpp
@@ -40,16 +40,11 @@ void TTEntry::save(Key k, Value v, Bound b, Depth d, Move m, Value ev) {
       move16 = (uint16_t)m;
 
   // Overwrite less valuable entries
-  if (  (k >> 48) != key16
-      || d / ONE_PLY > depth8 - 4
-      || b == BOUND_EXACT)
-  {
-      key16     = (uint16_t)(k >> 48);
-      value16   = (int16_t)v;
-      eval16    = (int16_t)ev;
-      genBound8 = (uint8_t)(TT.generation8 | b);
-      depth8    = (int8_t)(d / ONE_PLY);
-  }
+  key16     = (uint16_t)(k >> 48);
+  value16   = (int16_t)v;
+  eval16    = (int16_t)ev;
+  genBound8 = (uint8_t)(TT.generation8 | b);
+  depth8    = (int8_t)(d / ONE_PLY);
 }
 
 
