19240fd6d0df93ba0a62eed4159e1304faf0d073
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-12-24 14:00:46 +0100

    [WIP] Experiment

--- src/cluster.cpp
+++ src/cluster.cpp
@@ -124,23 +124,7 @@ bool getline(std::istream& input, std::string& str) {
       size = vec.size();
   }
 
-  // Some MPI implementations use busy-wait pooling, while we need yielding
-  static MPI_Request reqInput = MPI_REQUEST_NULL;
-  MPI_Ibcast(&size, 1, MPI_INT, 0, InputComm, &reqInput);
-  if (is_root())
-      MPI_Wait(&reqInput, MPI_STATUS_IGNORE);
-  else
-  {
-      while (true)
-      {
-          int flag;
-          MPI_Test(&reqInput, &flag, MPI_STATUS_IGNORE);
-          if (flag)
-              break;
-          else
-              std::this_thread::sleep_for(std::chrono::milliseconds(10));
-      }
-  }
+  MPI_Bcast(&size, 1, MPI_INT, 0, InputComm);
 
   if (!is_root())
       vec.resize(size);
