de2cc9ca8d552f6e28a9713c82eb7295a2ef4933
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-01-12 21:54:04 +0100

    drawMat. Bench: 3782840

--- src/search.cpp
+++ src/search.cpp
@@ -87,9 +87,18 @@ namespace {
 
   // Add a small random component to draw evaluations to keep search dynamic
   // and to avoid 3fold-blindness.
-  Value value_draw(Depth depth, Thread* thisThread) {
-    return depth < 4 ? VALUE_DRAW
-                     : VALUE_DRAW + Value(2 * (thisThread->nodes.load(std::memory_order_relaxed) % 2) - 1);
+  Value value_draw(Position& pos, Depth depth) {
+    if (depth < 4)
+        return VALUE_DRAW;
+    else 
+    {
+        Color us = pos.side_to_move();
+        Value matDiff = (pos.count<PAWN>( us) - pos.count<PAWN>(~us)) * PawnValueMg
+                       + pos.non_pawn_material( us) - pos.non_pawn_material(~us);
+        return   VALUE_DRAW
+               + Value(matDiff >= 0)
+               + Value(2 * (pos.this_thread()->nodes.load(std::memory_order_relaxed) % 2) - 1);
+     }
   }
 
   // Skill structure is used to implement strength limit
@@ -555,7 +564,7 @@ namespace {
         && !rootNode
         && pos.has_game_cycle(ss->ply))
     {
-        alpha = value_draw(depth, pos.this_thread());
+        alpha = value_draw(pos, depth);
         if (alpha >= beta)
             return alpha;
     }
@@ -605,7 +614,7 @@ namespace {
             || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
             return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos)
-                                                    : value_draw(depth, pos.this_thread());
+                                                    : value_draw(pos, depth);
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply+1), but if alpha is already bigger because
