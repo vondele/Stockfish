c29e5340967dd61e5243900c7191659e890a6343
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-03-17 11:28:39 +0100

    RQ only. Bench: 3650766

--- src/search.cpp
+++ src/search.cpp
@@ -1020,7 +1020,7 @@ moves_loop: // When in check, search starts from here
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
 
-          if (type_of(movedPiece) >= ROOK)
+          if (type_of(movedPiece) == ROOK || type_of(movedPiece) == QUEEN)
               r -= ONE_PLY;
 
           if (!captureOrPromotion)
