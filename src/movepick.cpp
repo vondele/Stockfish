b3aa4733e6342d31217b5629f50f54f7d4efe567
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-04-25 17:05:30 +0200

    enhance pawns with 50mr. Param 256. Bench: 3059176

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -116,7 +116,7 @@ void MovePicker::score() {
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2
-		   + ((pos.rule50_count() > 18 && type_of(pos.moved_piece(m)) == PAWN) ? 10692 : 0)  ;
+		   + (type_of(pos.moved_piece(m)) == PAWN ? pos.rule50_count() * 256 : 0);
 
       else // Type == EVASIONS
       {
