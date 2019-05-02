72b3a3076172732ec741b7af11f2d40003eacf9f
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-05-02 22:32:59 +0200

    Reduce less with much choice. Bench: 3327048

--- src/search.cpp
+++ src/search.cpp
@@ -1021,7 +1021,7 @@ moves_loop: // When in check, search starts from here
 
           if (   depth > 10 * ONE_PLY
               && (ss-1)->availableMoves > 35)
-              r += ONE_PLY;
+              r -= ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
