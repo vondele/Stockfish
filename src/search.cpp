dce72e3c545697a7ad7b76623a3091638858c8ad
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-05-02 14:36:16 +0200

    Consider the other piece. Take 2. Bench: 3281153

--- src/search.cpp
+++ src/search.cpp
@@ -1025,7 +1025,7 @@ moves_loop: // When in check, search starts from here
               r -= ONE_PLY;
 
           if (movedPiece == ttPiece && to_sq(move) == ttSquare)
-              r -= ONE_PLY;
+              r += ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
