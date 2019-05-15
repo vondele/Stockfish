bbbe8c085344895d9c20534e12c57a2a6bb5ed24
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-05-15 21:43:56 +0200

    SEE with counter. Take 2. Bench: 3339182

--- src/search.cpp
+++ src/search.cpp
@@ -985,7 +985,7 @@ moves_loop: // When in check, search starts from here
               if (!pos.see_ge(move, Value(-29 * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (!pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY + captCount / 4))) // (~20 Elo)
+          else if (!pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY - captCount / 4))) // (~20 Elo)
                   continue;
 	  else
 	       captCount++;
