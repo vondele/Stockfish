a3e2a4eea77db562f3c49e80a0e837a6d0451941
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-05-16 12:15:09 +0200

    Don't decrease in depth for sameMoves. Take 1 (fixed). Bench: 3721441

--- src/tt.cpp
+++ src/tt.cpp
@@ -45,13 +45,15 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
 
   // Overwrite less valuable entries
   if (  (k >> 48) != key16
-      || (sameMove ? d > depth() : d / ONE_PLY + 10 > depth8)
+      || d / ONE_PLY + 10 > depth8
       || b == BOUND_EXACT)
   {
       key16     = (uint16_t)(k >> 48);
       value16   = (int16_t)v;
       eval16    = (int16_t)ev;
       genBound8 = (uint8_t)(TT.generation8 | uint8_t(pv) << 2 | b);
+      if (sameMove)
+          d = std::max(d, depth());
       assert((d - DEPTH_NONE) / ONE_PLY >= 0);
       depth8    = (uint8_t)((d - DEPTH_NONE) / ONE_PLY);
   }
