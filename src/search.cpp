12cceffa06f1bcdd38e6faa15179087d49b2b95e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-05-23 07:51:41 +0200

    Remove depth condition. Bench: 3542244

--- src/search.cpp
+++ src/search.cpp
@@ -731,8 +731,7 @@ namespace {
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !PvNode
-        &&  depth < 7 * ONE_PLY
-        &&  eval - futility_margin(depth, improving) >= beta
+        &&  eval >= beta + futility_margin(depth, improving)
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
 
