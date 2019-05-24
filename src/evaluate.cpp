564353aa39442247d0fb490577878a32dccc721d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-05-24 20:18:26 +0200

    Take 2. Bench: 3234714

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -753,7 +753,7 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
     // that the endgame score will never change sign after the bonus.
-    int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg) + 2);
+    int v = ((eg > 0) - (eg < 0)) * std::max(complexity, std::min(0, -abs(eg) + 2));
 
     if (T)
         Trace::add(INITIATIVE, make_score(0, v));
