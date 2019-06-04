dbdc137cc9043aa28ffdc1850d85579b0ec4e937
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-06-04 16:53:28 +0200

    Take 2. Bench: 3555865

--- src/search.cpp
+++ src/search.cpp
@@ -1021,7 +1021,7 @@ moves_loop: // When in check, search starts from here
           if (ttPv)
               r -= 2 * ONE_PLY;
 
-	  if (ss->singularExtCount > 8)
+	  if (ss->singularExtCount > thisThread->rootDepth / 4)
               r += ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
