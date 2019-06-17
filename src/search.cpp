d15ac08ea5e4d0a97a8d1b418af339ebadcdfcc9
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-06-17 16:46:25 +0200

    Reductions. Take 4. Bench: 3897263

--- src/search.cpp
+++ src/search.cpp
@@ -906,9 +906,7 @@ moves_loop: // When in check, search starts from here
       // then that move is singular and should be extended. To verify this we do
       // a reduced search on all the other moves but the ttMove and if the
       // result is lower than ttValue minus a margin then we will extend the ttMove.
-      if (th.marked())
-	  extension = - ONE_PLY;
-      else if (    depth >= 8 * ONE_PLY
+      if (    depth >= 8 * ONE_PLY
           &&  move == ttMove
           && !rootNode
           && !excludedMove // Avoid recursive singular search
@@ -1035,6 +1033,9 @@ moves_loop: // When in check, search starts from here
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha))
       {
           Depth r = reduction(improving, depth, moveCount);
+
+	  if (th.marked())
+              r -= ONE_PLY;
 
           // Decrease reduction if position is or has been on the PV
           if (ttPv)
