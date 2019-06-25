07aab59496657b486675835d234369543796328a
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-06-25 22:10:47 +0200

    Take 1. Bench: 3321141

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -467,6 +467,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
+                 -   1 * pos.count<PAWN>()
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
