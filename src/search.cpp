ca297ab941a896012ff871df02518670dbecaedb
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-08-05 19:06:11 +0200

    Tweak. Take 3. Bench: 3956922

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= beta - 28 * depth / ONE_PLY + 217
+        &&  ss->staticEval >= beta - 42 * depth / ONE_PLY + 308
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
