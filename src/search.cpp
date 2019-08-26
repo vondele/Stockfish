57f780c7b86e303348f450b7214b191409f0f50e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-08-26 23:56:35 +0200

    Take 4. Bench: 3773869

--- src/search.cpp
+++ src/search.cpp
@@ -1053,7 +1053,7 @@ moves_loop: // When in check, search starts from here
                   continue;
           }
           else if (  (!givesCheck || !extension)
-                   && !pos.see_ge(move, Value(thisThread->checkIndex < 12800 ? -199 : -300) * (depth / ONE_PLY))) // (~20 Elo)
+                   && !pos.see_ge(move, Value(thisThread->checkIndex < 12800 ? -199 : -250) * (depth / ONE_PLY))) // (~20 Elo)
                   continue;
       }
 
