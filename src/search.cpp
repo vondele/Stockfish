13135e6290e2e1986eac014f0d7bf1d9d6877fc5
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-09-06 18:53:29 +0200

    Only early. Take 2. Bench: 3726512

--- src/search.cpp
+++ src/search.cpp
@@ -1093,7 +1093,7 @@ moves_loop: // When in check, search starts from here
               r -= 2 * ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
-          if ((ss-1)->moveCount > 15 && moveCount <= 3)
+          if ((ss-1)->moveCount > 15 && moveCount <= 5)
               r -= ONE_PLY;
 
           // Decrease reduction if move has been singularly extended
