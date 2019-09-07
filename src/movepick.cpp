67166d953cb6d2b3ee857ebfc67396969af2b0d6
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-09-07 10:00:18 +0200

    capture tosq, bench: 4257668

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -109,7 +109,7 @@ void MovePicker::score() {
       if (Type == CAPTURES)
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
-                   + 256 * (ttMove && to_sq(ttMove) == to_sq(m)) ;
+                   + 512 * (ttMove && to_sq(ttMove) == to_sq(m)) ;
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
