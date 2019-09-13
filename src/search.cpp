23c77ee8c9ac482f9f26475b6edfb07b812a3437
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-09-13 23:41:12 +0200

    Take 9. Bench: 4037798

--- src/search.cpp
+++ src/search.cpp
@@ -1036,7 +1036,7 @@ moves_loop: // When in check, search starts from here
               lmrDepth /= ONE_PLY;
 
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1) + (thisThread->rootDepth + 6 < thisThread->previousCompletedDepth)
+              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1) - (thisThread->rootDepth + 6 < thisThread->previousCompletedDepth)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
