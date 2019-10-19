2662a834d335f0dbe98bc55b109ae7645e465c08
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-10-19 22:43:21 +0200

    Final tune. Bench: 4566120

--- src/search.cpp
+++ src/search.cpp
@@ -67,8 +67,6 @@ namespace {
     return Value(198 * (d - improving));
   }
 
-  int capturePruneMargins[4] = {-200, -179, -198, -199};
-
   // Reductions lookup table, initialized at startup
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
@@ -1059,7 +1057,7 @@ moves_loop: // When in check, search starts from here
                   continue;
           }
           else if (  !(givesCheck && extension)
-                   && !pos.see_ge(move, Value(capturePruneMargins[std::min(captureCount,3)]) * depth)) // (~20 Elo)
+                   && !pos.see_ge(move, -(captureCount == 1 ? Value(175) : Value(199)) * depth)) // (~20 Elo)
                   continue;
       }
 
