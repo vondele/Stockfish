215177720369d9371f03775e00a79f06d677b243
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-10-23 07:28:13 +0200

    Take 4. Bench: 4218940

--- src/search.cpp
+++ src/search.cpp
@@ -1287,7 +1287,7 @@ moves_loop: // When in check, search starts from here
                                stat_bonus(depth + (bestValue > beta + PawnValueMg)));
 
         update_capture_stats(pos, bestMove, capturesSearched, captureCount,
-                             (stat_bonus(depth + 1) + stat_bonus(depth + 1 + bestMoveGivesCheck)) / 2);
+                             stat_bonus(depth + 1 + 2 * bestMoveGivesCheck));
 
         // Extra penalty for a quiet TT or main killer move in previous ply when it gets refuted
         if (   ((ss-1)->moveCount == 1 || ((ss-1)->currentMove == (ss-1)->killers[0]))
