a8467cc0993a73702f8ac8d23dde5cf5865b5e69
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-10-31 14:38:46 +0100

    Take 4. Bench: 4352330

--- src/search.cpp
+++ src/search.cpp
@@ -1100,7 +1100,7 @@ moves_loop: // When in check, search starts from here
               r -= 2;
 
 	  if (!ttHit)
-	      r -= 1;
+	      r += 1;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
