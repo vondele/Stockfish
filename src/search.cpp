6d940a662eb8d3742b516d766119d377bd1e1273
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-04 19:06:48 +0100

    Time contempt. Take 3. Bench: 4312945

--- src/search.cpp
+++ src/search.cpp
@@ -370,7 +370,7 @@ void Thread::search() {
   double bt = Limits.time[BLACK] + 16 * Limits.inc[BLACK];
 
   if (wt && bt)
-     ct += int(20 * clamp(std::log2(us == WHITE ? wt / bt : bt / wt), -2., 2.)) * PawnValueEg / 100;
+     ct += int(10 * clamp(std::log2(us == WHITE ? wt / bt : bt / wt), -2., 2.)) * PawnValueEg / 100;
 
   // In analysis mode, adjust contempt in accordance with user preference
   if (Limits.infinite || Options["UCI_AnalyseMode"])
