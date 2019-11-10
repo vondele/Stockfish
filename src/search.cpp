b78f89f03b5f40470b20e0725e803502b42fc97d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-10 21:44:01 +0100

    Take 2. Bench: 4337376

--- src/search.cpp
+++ src/search.cpp
@@ -796,7 +796,7 @@ namespace {
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !PvNode
-        &&  depth < 7
+        &&  depth < 8
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
