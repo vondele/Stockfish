6926816881e537437b167e403fb06f6d4c34317b
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-12 16:52:59 +0100

    Limit singularBeta to beta. Bench: 4290255

--- src/search.cpp
+++ src/search.cpp
@@ -972,7 +972,7 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3
           &&  pos.legal(move))
       {
-          Value singularBeta = ttValue - 2 * depth;
+          Value singularBeta = std::min(beta, ttValue - 2 * depth);
           Depth halfDepth = depth / 2;
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, halfDepth, cutNode);
