21e6af05792f214699dd58af2fdb8419ee03ba95
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-16 13:05:25 +0100

    Use 50mr counter to adjust see threshold of bad captures. Take 1. Bench: 4480477

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -174,7 +174,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(*cur, Value(-55 * cur->value / 1024)) ?
+                       return pos.see_ge(*cur, Value(-55 * cur->value / 1024 - 2 * pos.rule50_count())) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
