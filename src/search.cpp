3215da5059f7648f3dd27e0bc7573e01dfea2a32
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-18 16:16:00 +0100

    tweak threshold. Take 1. Param 536. Bench: 5005936

--- src/search.cpp
+++ src/search.cpp
@@ -1090,7 +1090,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           // Decrease reduction if the ttHit running average is large
-          if (thisThread->ttHitAverage > 544 * ttHitAverageResolution * ttHitAverageWindow / 1024)
+          if (thisThread->ttHitAverage > 536 * ttHitAverageResolution * ttHitAverageWindow / 1024)
               r--;
 
           // Reduction if other threads are searching this position.
