45d20b483cd704e86ca4c5c616f618e104c3e046
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-20 23:04:22 +0100

    LMR and inCheckAverage. Take 2. Bench: 5109799

--- src/search.cpp
+++ src/search.cpp
@@ -1098,7 +1098,8 @@ moves_loop: // When in check, search starts from here
           if (thisThread->ttHitAverage > 544 * ttHitAverageResolution * ttHitAverageWindow / 1024)
               r--;
 
-	  if (thisThread->inCheckAverage > 130 * inCheckAverageResolution * inCheckAverageWindow / 1024)
+	  if (thisThread->inCheckAverage > 130 * inCheckAverageResolution * inCheckAverageWindow / 1024
+              && captureOrPromotion)
 	      r--;
 
           // Reduction if other threads are searching this position.
