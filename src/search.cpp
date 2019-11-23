cb4d0ed75cbd299c99a97ef79cd0aee051fa7fed
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-23 09:41:30 +0100

    Take 4. Bench: 4822810

--- src/search.cpp
+++ src/search.cpp
@@ -1099,7 +1099,7 @@ moves_loop: // When in check, search starts from here
           if (thisThread->ttHitAverage > 544 * ttHitAverageResolution * ttHitAverageWindow / 1024)
               r--;
 
-	  if (thisThread->inCheckAverage > 130 * inCheckAverageResolution * inCheckAverageWindow / 1024)
+	  if (thisThread->inCheckAverage > 100 * inCheckAverageResolution * inCheckAverageWindow / 1024)
 	      r++;
 
           // Reduction if other threads are searching this position.
