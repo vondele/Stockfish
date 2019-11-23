2bb05abc13c5089093d9f58ab48bed23fb317283
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-23 18:03:16 +0100

    Tune. Take 2. Bench: 4772663

--- src/search.cpp
+++ src/search.cpp
@@ -1085,7 +1085,7 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
-              || thisThread->ttHitAverage < 354 * ttHitAverageResolution * ttHitAverageWindow / 1024))
+              || thisThread->ttHitAverage < 414 * ttHitAverageResolution * ttHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
 
