0892cf64243066a22717ee3dca143439b5805dd8
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-12-02 08:11:52 +0100

    nmp & tempo. Take 2. Bench: 4954519

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -827,7 +827,7 @@ namespace {
     }
 
     return  (pos.side_to_move() == WHITE ? v : -v) // Side to move point of view
-           + (pos.non_pawn_material(WHITE) == pos.non_pawn_material(BLACK) ? 2 : -2) + Eval::Tempo;
+           + (pos.non_pawn_material(WHITE) == pos.non_pawn_material(BLACK) ? -2 : 2) + Eval::Tempo;
   }
 
 } // namespace
