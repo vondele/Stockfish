b4a6db699857415367e402b5c6576f52196afd66
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-12-13 22:58:21 +0100

    Adjust max MTG by 50mr. Take 3. Bench: 5371271

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -106,8 +106,8 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply, int c50
 
   startTime = limits.startTime;
   optimumTime = maximumTime = std::max(limits.time[us], minThinkingTime);
-  int moveHorizon = std::max(10, (100 - c50mr) / 2);
-  const int maxMTG = limits.movestogo ? std::min(limits.movestogo, moveHorizon) : moveHorizon; 
+  int moveHorizon = 50 - c50mr / 3;
+  const int maxMTG = limits.movestogo ? std::min(limits.movestogo, moveHorizon) : moveHorizon;
 
   // We calculate optimum time usage for different hypothetical "moves to go" values
   // and choose the minimum of calculated search time values. Usually the greatest
