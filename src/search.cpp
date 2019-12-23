c730e534a5a1051c91e80122aef47d421405bccb
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-12-23 10:27:52 +0100

    Take 15. Bench: 5289658

--- src/search.cpp
+++ src/search.cpp
@@ -447,7 +447,7 @@ void Thread::search() {
           if (rootDepth >= 4)
           {
               Value previousScore = rootMoves[pvIdx].previousScore;
-              delta = Value(21 + abs(previousScore) / 128 + 1 * pvDraw);
+              delta = Value(21 + abs(previousScore) / 128 + 4 * pvDraw);
               alpha = std::max(previousScore - delta,-VALUE_INFINITE);
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
