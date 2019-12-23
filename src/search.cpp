fe1f8a97f74ecee6ff80560724a68854f0c5d614
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-12-23 20:58:39 +0100

    LMR on thread marked pos. Bench: 5371271

--- src/search.cpp
+++ src/search.cpp
@@ -1104,6 +1104,7 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
+              || th.marked()
               || thisThread->ttHitAverage < 384 * ttHitAverageResolution * ttHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
