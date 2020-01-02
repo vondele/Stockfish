c8eff9015dfda82c67a7edbf3f53c7a93d3ac49e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-01-02 18:08:01 +0100

    Take 6. Bench: 4719090

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -86,7 +86,7 @@ namespace {
   constexpr int BishopSafeCheck = 635;
   constexpr int KnightSafeCheck = 790;
 
-  constexpr int complexityParams[8] = {14, 16, 9, 25, 88, 69, 26, 47};
+  constexpr int complexityParams[8] = {14, 16, 8, 18, 89, 66, 29, 29};
 
 #define S(mg, eg) make_score(mg, eg)
 
