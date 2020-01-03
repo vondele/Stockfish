4b89b2904814ccb44e1bd290feeebef96f61d68b
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-01-03 17:18:52 +0100

    Some breadcrumb tweaks. Bench: 5180012

--- src/search.cpp
+++ src/search.cpp
@@ -108,7 +108,7 @@ namespace {
     std::atomic<Thread*> thread;
     std::atomic<Key> key;
   };
-  std::array<Breadcrumb, 1024> breadcrumbs;
+  std::array<Breadcrumb, 2048> breadcrumbs;
 
   // ThreadHolding structure keeps track of which thread left breadcrumbs at the given
   // node for potential reductions. A free node will be marked upon entering the moves
