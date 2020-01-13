884996b7037191269ef12996c0c3613c08ecd10b
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-01-13 15:46:40 +0100

    Fail low LMR. Take 1. Bench: 4777034

--- src/search.cpp
+++ src/search.cpp
@@ -1125,6 +1125,9 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
+	  if (rootNode && bestValue < alpha)
+         	  r--;
+
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 500 * ttHitAverageResolution * ttHitAverageWindow / 1024)
               r--;
