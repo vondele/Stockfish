f435813d3b6686281f2d9f462eec79894a629844
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-03-28 17:44:33 +0100

    Just material counting..

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -772,6 +772,16 @@ namespace {
 
     assert(!pos.checkers());
 
+    int vv =  pos.count<PAWN>(WHITE) - pos.count<PAWN>(BLACK)
+            + (pos.count<BISHOP>(WHITE) - pos.count<BISHOP>(BLACK)) * 3
+            + (pos.count<KNIGHT>(WHITE) - pos.count<KNIGHT>(BLACK)) * 3
+            + (pos.count<ROOK>(WHITE) - pos.count<ROOK>(BLACK)) * 5
+            + (pos.count<QUEEN>(WHITE) - pos.count<QUEEN>(BLACK)) * 9;
+
+    vv *= PawnValueEg;
+
+    return  Value(pos.side_to_move() == WHITE ? vv : -vv);
+
     // Probe the material hash table
     me = Material::probe(pos);
 
