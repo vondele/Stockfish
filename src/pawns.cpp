9035ee44d234d4c737b97f6d08c13e1036d73632
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-03-29 22:12:11 +0200

    Used passed pawns if we have them. Bench: 4850922

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -239,7 +239,7 @@ Score Entry::do_king_safety(const Position& pos) {
       shelter = std::max(shelter, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)), compare);
 
   // In endgame we like to bring our king near our closest pawn
-  Bitboard pawns = pos.pieces(Us, PAWN);
+  Bitboard pawns = passedPawns[Us] ? passedPawns[Us] : pos.pieces(Us, PAWN);
   int minPawnDist = pawns ? 8 : 0;
 
   if (pawns & PseudoAttacks[KING][ksq])
