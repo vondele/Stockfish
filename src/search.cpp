2dfd1b3f10044b866b026340510f79f93eafffae
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-04-08 07:48:31 +0200

    Tweak. Take 1. Bench: 4736461

--- src/search.cpp
+++ src/search.cpp
@@ -439,7 +439,7 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + (102 - ct / 2) * previousScore / (abs(previousScore) + 157);
+              int dct = ct + (96 - 164 * ct / 256) * previousScore / (abs(previousScore) + 170);
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
