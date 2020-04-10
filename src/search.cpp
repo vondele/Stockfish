1f3fd1815ee885f322b93f0f0165918a1d4ab398
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-04-10 13:35:20 +0200

    Play with random reduction. Take 2. Bench: 4275129

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop: // When in check, search starts from here
           if (singularLMR)
               r -= 1 + formerPv;
 
-          r += (2 * (thisThread->nodes & 1) - 1) * moveCount / 8;
+          r += (thisThread->nodes & 1) * moveCount / 8;
 
           if (!captureOrPromotion)
           {
