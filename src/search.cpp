7a40158e15c5dd2e20c2c8c43066c29cecce9de3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-04-11 11:12:15 +0200

    Take 4. Bench: 4499282

--- src/search.cpp
+++ src/search.cpp
@@ -1110,7 +1110,7 @@ moves_loop: // When in check, search starts from here
       if (   move == ttMove
           && pos.rule50_count() > 60
           && (captureOrPromotion || type_of(movedPiece) == PAWN))
-          extension = 1;
+          extension = 2;
 
       // Add extension to new depth
       newDepth += extension;
