7a6f9a8ff20de2de79fe65f68bd5800e0422b1ec
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-04-18 22:17:41 +0200

    qs draw. Take 3. Bench: 5007002

--- src/search.cpp
+++ src/search.cpp
@@ -1432,7 +1432,7 @@ moves_loop: // When in check, search starts from here
     // Check for an immediate draw or maximum ply reached
     if (   pos.is_draw(ss->ply)
         || ss->ply >= MAX_PLY)
-        return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos) : VALUE_DRAW - 2;
+        return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos) : VALUE_DRAW + 2;
 
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
 
