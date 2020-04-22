1e56605a937ff68556fc8a6b99b92154dd1df042
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-04-22 23:39:09 +0200

    Another check sequence test. Bench: 4693514

--- src/search.cpp
+++ src/search.cpp
@@ -627,7 +627,7 @@ namespace {
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue;
     bool ttHit, ttPv, formerPv, givesCheck, improving, didLMR, priorCapture;
-    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, ttCapture, singularLMR;
+    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, ttCapture, ttCheck, singularLMR;
     Piece movedPiece;
     int moveCount, captureCount, quietCount;
 
@@ -970,6 +970,7 @@ moves_loop: // When in check, search starts from here
     value = bestValue;
     singularLMR = moveCountPruning = false;
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
+    ttCheck = ttMove && pos.gives_check(ttMove);
 
     // Mark this node as being searched
     ThreadHolding th(thisThread, posKey, ss->ply);
@@ -1178,6 +1179,9 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if position is or has been on the PV (~10 Elo)
           if (ttPv)
               r -= 2;
+
+          if (ttCheck && (ss-1)->inCheck && (ss-3)->inCheck && !givesCheck)
+              r++;
 
           if (moveCountPruning && !formerPv)
               r++;
