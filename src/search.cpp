a0f1afeb327ff4bcac5de3e01b68b17a8c9d8c30
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-04-23 19:03:17 +0200

    capture after inCheck. Bench: 4125502

--- src/search.cpp
+++ src/search.cpp
@@ -1045,6 +1045,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
+                  && !(ss-1)->inCheck
                   && lmrDepth < 1
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
