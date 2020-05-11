920865c4ba9dcc1029a85db9583f6ce700d64eba
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-05-11 19:08:13 +0200

    simplify

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -739,7 +739,6 @@ namespace {
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    -  2 * pos.rule50_count()
                     -110 ;
 
     Value mg = mg_value(score);
