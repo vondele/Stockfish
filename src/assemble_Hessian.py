a8a6d5a9e507d58ddcc4e252e16d227c5a8a7890
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-05-20 13:49:18 +0200

    Add python file to run and analyze output
    (needs a varied book, not added)
    
    Typical output:
    
    ```
    Starting stockfish
    
    Found 4617662 matching output lines
    
    Variables:
    ['mWeakQueen', 'eWeakQueen', 'mWeakQueenProtection', 'coef1']
    
    Hessian:
    [[ 8.21766060e-02  1.09108664e-02  2.85526193e-03 -4.50610604e-04]
     [ 1.09108664e-02  6.38933661e-03  8.39296488e-04 -1.27340027e-04]
     [ 2.85526193e-03  8.39296488e-04  2.80866064e-01  3.04453287e-03]
     [-4.50610604e-04 -1.27340027e-04  3.04453287e-03  7.74931877e-02]]
    
    Diagonal:  [0.08217660601462329, 0.006389336611263206, 0.2808660638221601, 0.07749318770434117]
    Lenghtscales:  [3.4883969634305103, 12.510426496489284, 1.886906437035541, 3.5922639250628006]
    Estimated d for intervals [-d, d] around optimum yielding 2 Elo change:
    d:  [45, 161, 25, 46]
    
    Eigenvalues:  [0.08371357 0.00484907 0.28095605 0.07740651]
    Lenghtscales:  [3.4562254580239045, 14.360547121278744, 1.8866042546827155, 3.5942745673689345]
    
    Eigenvectors:
    [[ 9.86845974e-01 -1.39652754e-01 -1.45259775e-02  8.01319415e-02]
     [ 1.39234716e-01  9.90198842e-01 -3.62643842e-03  1.03342003e-02]
     [-1.36337166e-02 -1.57661871e-03 -9.99776508e-01 -1.60801277e-02]
     [-8.10113547e-02  9.35560935e-04 -1.49257960e-02  9.96600976e-01]]
    
    ```

---
+++ src/assemble_Hessian.py
@@ -0,0 +1,74 @@
+from subprocess import Popen, PIPE
+import ast
+import numpy as np
+import math
+from numpy import linalg as LA
+
+print("Starting stockfish")
+# book must contain a wide variety of positions, not just openings. Eval is called on these EPDs only.
+command = "./stockfish bench 128 1 1 variedbook.epd eval 2>&1 | grep Deriv"
+process = Popen(command, shell=True, stdout=PIPE)
+output  = process.communicate()[0]
+if process.returncode != 0:
+   sys.exit("failed to execute command: %s\n" % command)
+
+dicts = []
+for line in output.decode("utf-8").splitlines():
+    if line.startswith("Derivatives:"):
+       dicts.append(ast.literal_eval(line[13:])) 
+
+Ndicts = len(dicts)
+print()
+print("Found %d matching output lines" % Ndicts)
+keys = [k for k in dicts[0]]
+Nkeys = len(keys)
+
+print()
+print("Variables: ")
+print(keys)
+
+Hessian = np.zeros(Nkeys * Nkeys, dtype=np.float64).reshape(Nkeys, Nkeys)
+
+for d in dicts:
+  for i in range(0, Nkeys):
+    for j in range(0, Nkeys):
+      Hessian[i, j] = Hessian[i, j] + d[keys[i]] * d[keys[j]] / Ndicts
+
+print()
+print("Hessian: ")
+print(Hessian)
+
+print()
+diag = [Hessian[i, i] for i in range(0, Nkeys)]
+print("Diagonal: ", diag)
+lengthscales = [ 1/math.sqrt(Hessian[i, i])  for i in range(0, Nkeys)]
+print("Lenghtscales: ", lengthscales)
+
+# based on the coef1 testcase of https://github.com/glinscott/fishtest/issues/535
+print("Estimated d for intervals [-d, d] around optimum yielding 2 Elo change:")
+print("d: ", [math.ceil(l * 12.8) for l in lengthscales])
+
+
+e, v = LA.eigh(Hessian)
+
+# reorder close to unit matrix
+available = list(range(0, Nkeys))
+reorder = []
+for i in range(0, Nkeys):
+    maxval, maxind = -1, -1
+    for j in available:
+        if abs(v[i, j]) > maxval:
+           maxval, maxind = abs(v[i ,j]), j
+    reorder.append(maxind)
+    available.remove(maxind)
+
+v = v[:, reorder]
+e = e[reorder]
+
+print()
+print("Eigenvalues: ", e)
+lengthscales = [ 1/math.sqrt(e[i]) if e[i] > 0 else 0 for i in range(0, Nkeys)]
+print("Lenghtscales: ", lengthscales)
+print()
+print("Eigenvectors: ")
+print(v)
