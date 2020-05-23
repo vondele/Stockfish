fe58f94740649a03bb2a4881dfbf2902cb679bcb
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-05-23 18:17:03 +0200

    Reductions with 3fold count. Take 5. Bench: 5163872

--- src/search.cpp
+++ src/search.cpp
@@ -1199,7 +1199,7 @@ moves_loop: // When in check, search starts from here
           if (thisThread->count3fold>0)
           {
              r--;
-             thisThread->count3fold -= 2;
+             thisThread->count3fold -= 1;
           }
 
           if (moveCountPruning && !formerPv)
@@ -1455,7 +1455,10 @@ moves_loop: // When in check, search starts from here
     // Check for an immediate draw or maximum ply reached
     if (   pos.is_draw(ss->ply)
         || ss->ply >= MAX_PLY)
+    {
+        ++thisThread->count3fold;
         return (ss->ply >= MAX_PLY && !ss->inCheck) ? evaluate(pos) : VALUE_DRAW;
+    }
 
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
 
