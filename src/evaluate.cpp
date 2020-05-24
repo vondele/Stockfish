425881b43b222686c04641496ca99fade16d2672
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-05-24 19:30:54 +0200

    Simplify. Take 2. Bench: 4839018

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -454,12 +454,12 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
-                 // +  98 * popcount(pos.blockers_for_king(Us))
+                 +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
-                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
+                 // - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
                  +  37;
