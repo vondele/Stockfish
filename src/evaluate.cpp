b9ac986465add25b9df65c78bc8c947a46e49a5b
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-05-24 12:38:47 +0200

    Take 1. Bench: 4764857

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -734,7 +734,7 @@ namespace {
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
-                    + 51 * !pos.non_pawn_material()
+                    // + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
                     -  2 * pos.rule50_count()
                     -110 ;
