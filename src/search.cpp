ee9c89699079596582de900990e20bd3a3ac9857
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-04 21:21:55 +0200

    Take 2. More reduction. Bench: 4561590

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           if (256 * msb(ss->moveCountHistory) > 350 * thisThread->rootDepth)
-              r--;
+              r++;
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 500 * TtHitAverageResolution * TtHitAverageWindow / 1024)
