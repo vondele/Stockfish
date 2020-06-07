19d48122d3af545145e25f0804ac60d4be7d8fb8
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-07 11:56:59 +0200

    Add rnd to Lazy eval. Take 1. Bench: 4700809

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -817,7 +817,7 @@ namespace {
     score += pe->pawn_score(WHITE) - pe->pawn_score(BLACK);
 
     // Early exit if score is high
-    Value v = (mg_value(score) + eg_value(score)) / 2;
+    Value v = (mg_value(score) + eg_value(score)) / 2 + Value((pos.this_thread()->nodes & 4) - 2);
     if (abs(v) > LazyThreshold + pos.non_pawn_material() / 64)
        return pos.side_to_move() == WHITE ? v : -v;
 
