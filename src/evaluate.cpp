750477896e4768e46aa43425a17acbb1dac2ae54
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-08 09:15:28 +0200

    Take 4. Bench: 4834235

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,10 +743,10 @@ namespace {
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
-                    - 32 * !pos.count<QUEEN>()
+                    - 16 * !pos.count<QUEEN>()
                     - 43 * almostUnwinnable
                     -  2 * pos.rule50_count()
-                    -100 ;
+                    -105 ;
 
     Value mg = mg_value(score);
     Value eg = eg_value(score);
