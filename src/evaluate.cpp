119c470fc072c1e37b3a8e155eabbeac52724e47
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-08 08:30:56 +0200

    WIP

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,6 +743,7 @@ namespace {
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
+                    + 10 * pos.count<QUEEN>()
                     - 43 * almostUnwinnable
                     -  2 * pos.rule50_count()
                     -110 ;
