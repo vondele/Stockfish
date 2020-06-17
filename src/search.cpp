aeb363e941c5ad6040374955663facde1014d0ff
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-17 20:07:10 +0200

    Adjust reductions with alpha. Take 2. Bench: 4423661

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           if (alpha > 3 * PawnValueMg / 2)
-              r--;
+              r++;
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 473 * TtHitAverageResolution * TtHitAverageWindow / 1024)
