26b021a1701483dc2c17c464d7c32c61082c9eaa
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-17 22:26:27 +0200

    Take 3. Bench: 4438407

--- src/search.cpp
+++ src/search.cpp
@@ -1148,7 +1148,7 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (alpha > 3 * PawnValueMg / 2)
+          if (alpha > 2 * PawnValueMg / 2)
               r++;
 
           // Decrease reduction if the ttHit running average is large
