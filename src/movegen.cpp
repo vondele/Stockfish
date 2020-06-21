3423873260460121bfca6e83741f948da235dc1e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-21 21:53:00 +0200

    Underpromotions. Take 1. Bench: 4462162

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -33,9 +33,9 @@ namespace {
 
     if (Type == QUIETS || Type == EVASIONS || Type == NON_EVASIONS)
     {
+        *moveList++ = make<PROMOTION>(to - D, to, KNIGHT);
         *moveList++ = make<PROMOTION>(to - D, to, ROOK);
         *moveList++ = make<PROMOTION>(to - D, to, BISHOP);
-        *moveList++ = make<PROMOTION>(to - D, to, KNIGHT);
     }
 
     // Knight promotion is the only promotion that can give a direct check
