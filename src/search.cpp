f3b79d05dd3acbb03a9b2cd86563c30f43ec7f87
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-28 08:54:44 +0200

    Make fraction time used available in search. Use for shallow depth pruning. Take 3 Param 16 Bench: 4789930

--- src/search.cpp
+++ src/search.cpp
@@ -988,7 +988,7 @@ moves_loop: // When in check, search starts from here
       // Step 13. Pruning at shallow depth (~200 Elo)
       if (  !rootNode
           && pos.non_pawn_material(us)
-          && Threads.timeUsedFraction.load(std::memory_order_relaxed) > 8
+          && Threads.timeUsedFraction.load(std::memory_order_relaxed) > 16
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
