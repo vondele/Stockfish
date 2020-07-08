220c835b62c8c04acc13569985a78b6490e9d87f
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-07-08 21:19:45 +0200

    Take 2. Bench: 4315606

--- src/search.cpp
+++ src/search.cpp
@@ -915,7 +915,7 @@ namespace {
     }
 
     // Step 11. Internal iterative deepening (~1 Elo)
-    if (depth >= 7 && (!ttMove || tte->depth() < depth - 7))
+    if (depth >= 7 && (!ttMove || tte->depth() < depth - 8))
     {
         search<NT>(pos, ss, alpha, beta, depth - 7, cutNode);
 
