8b7fbb7ef9d893fe7ef13d99a50b9fecac798a6e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-08-14 21:55:48 +0200

    Reduce EP less. Bench: 3829900

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop: // When in check, search starts from here
           else
           {
             // Increase reduction for captures/promotions if late move and at low depth
-            if (depth < 8 && moveCount > 2)
+            if (depth < 8 && moveCount > 2 && type_of(move) != ENPASSANT)
                 r++;
 
             // Unless giving check, this capture is likely bad
