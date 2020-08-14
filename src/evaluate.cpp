a9392600c51e6c977503bc7a2fc14168d9af4cad
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-08-14 22:58:03 +0200

    Classical on capture. Take 1. Bench: 4526923

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -939,6 +939,7 @@ make_v:
 Value Eval::evaluate(const Position& pos) {
 
   bool classical = !Eval::useNNUE
+                ||  pos.rule50_count() < 1
                 ||  abs(eg_value(pos.psq_score())) >= NNUEThreshold;
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
