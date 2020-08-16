71a5cea476098315231affb2c71570270a13d231
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-08-17 00:02:31 +0200

    Update delta rule. Take 2. Bench: 4515387

--- src/search.cpp
+++ src/search.cpp
@@ -467,7 +467,7 @@ void Thread::search() {
                   break;
               }
 
-              delta += delta / 5 + 5;
+              delta += delta / 4 + 4;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
