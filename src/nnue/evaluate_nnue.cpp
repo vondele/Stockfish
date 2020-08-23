ffa4439e61dd90ef8aa1081ad5868b29af355cb6
J. Oster <osterj165@googlemail.com>
2020-08-23 11:52:40 +0200

    Use std::clamp. Bench: 3791848

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -174,7 +174,7 @@ namespace Eval::NNUE {
   Value evaluate(const Position& pos) {
 
     Value v = ComputeScore(pos, false);
-    v = Utility::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
+    v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
     return v;
   }
