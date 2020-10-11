674014b9a8dd07bf033bcc0ea84fa48325c8dab3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-10-11 20:04:34 +0200

    New net nn-558c6f776a0d.nnue. Bench: 3753178

--- src/evaluate.h
+++ src/evaluate.h
@@ -37,7 +37,7 @@ namespace Eval {
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
 
-  #define EvalFileDefaultName   "nn-6046c4a62935.nnue"
+  #define EvalFileDefaultName   "nn-558c6f776a0d.nnue"
 
   namespace NNUE {
 
