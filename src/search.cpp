1957c4b917bd2cd97a3d99b8a146b98b44505b13
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-10-30 18:43:52 +0100

    Add a little bonus. Take 2. Bench: 3550082

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop: // When in check, search starts from here
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
           if (!captureOrPromotion && value > alpha)
-              update_continuation_histories(ss, movedPiece, to_sq(move), stat_bonus(d));
+              update_continuation_histories(ss, movedPiece, to_sq(move), stat_bonus(d - 1));
 
           doFullDepthSearch = value > alpha && d != newDepth;
 
