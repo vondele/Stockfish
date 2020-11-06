1c95e53aa0b9fad06acad72f8d5bd174f78bafd2
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-11-06 22:32:18 +0100

    Repeated. Bench: 3648343

--- src/search.cpp
+++ src/search.cpp
@@ -1021,7 +1021,7 @@ moves_loop: // When in check, search starts from here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount) - 2 * pos.has_repeated(), 0);
 
           if (   !captureOrPromotion
               && !givesCheck)
