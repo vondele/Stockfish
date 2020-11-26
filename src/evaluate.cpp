1457c8b048db3eecc347ce8ef8a4eced2bfffabc
J. Oster <osterj165@googlemail.com>
2020-11-26 11:59:57 +0100

    use classical for certain endgames.
    STC https://tests.stockfishchess.org/tests/view/5fbc64c067cbf42301d6b1d6
    LLR: 2.97 (-2.94,2.94) {-0.25,1.25}
    Total: 53360 W: 5223 L: 5024 D: 43113
    Ptnml(0-2): 184, 3877, 18390, 4014, 215
    
    LTC https://tests.stockfishchess.org/tests/view/5fbc97f267cbf42301d6b1ee
    LLR: 2.96 (-2.94,2.94) {0.25,1.25}
    Total: 126472 W: 5111 L: 4766 D: 116595
    Ptnml(0-2): 50, 4032, 54749, 4333, 72
    
    closes #3240
    
    bench: 3820648

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1040,16 +1040,17 @@ Value Eval::evaluate(const Position& pos) {
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
       Value psq = Value(abs(eg_value(pos.psq_score())));
-      int   r50 = 16 + pos.rule50_count();
-      bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
-      bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
+      int r50 = 16 + pos.rule50_count();
+      bool largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
+      bool classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
+      bool strongClassical = pos.non_pawn_material() < 2 * RookValueMg && pos.count<PAWN>() < 2;
 
-      v = classical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
+      v = (classical || strongClassical) ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
 
       // If the classical eval is small and imbalance large, use NNUE nevertheless.
       // For the case of opposite colored bishops, switch to NNUE eval with
       // small probability if the classical eval is less than the threshold.
-      if (   largePsq
+      if (   largePsq && !strongClassical
           && (   abs(v) * 16 < NNUEThreshold2 * r50
               || (   pos.opposite_bishops()
                   && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
