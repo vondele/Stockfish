4e7b5c1de31d2fe497ada048e3570352eed2cd8e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-01-23 12:25:09 +0100

    All but L1. Bench: 4343624

--- src/nnue/layers/affine_transform.h
+++ src/nnue/layers/affine_transform.h
@@ -71,7 +71,7 @@ namespace Eval::NNUE::Layers {
         biases_[i] = (read_little_endian<BiasType>(stream) / 2) * 2;
       for (std::size_t i = 0; i < kOutputDimensions * kPaddedInputDimensions; ++i)
 #if !defined (USE_SSSE3)
-        if (kOutputDimensions == 1)
+        if (kPaddedInputDimensions >= 256)
            weights_[i] =  read_little_endian<WeightType>(stream);
         else
            weights_[i] = (read_little_endian<WeightType>(stream) / 2) * 2;
@@ -80,7 +80,7 @@ namespace Eval::NNUE::Layers {
           (i / 4) % (kPaddedInputDimensions / 4) * kOutputDimensions * 4 +
           i / kPaddedInputDimensions * 4 +
           i % 4
-        ] = kOutputDimensions == 1 ? read_little_endian<WeightType>(stream) : (read_little_endian<WeightType>(stream) / 2) * 2;
+        ] = kPaddedInputDimensions >= 256  ? read_little_endian<WeightType>(stream) : (read_little_endian<WeightType>(stream) / 2) * 2;
 
       // Determine if eights of weight and input products can be summed using 16bits
       // without saturation. We assume worst case combinations of 0 and 127 for all inputs.
