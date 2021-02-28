66547821bafe5d7d8906c7f8ba4a1190a287d9d2
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-02-28 18:55:18 +0100

    Take 4. Bench: 4070563

--- src/search.cpp
+++ src/search.cpp
@@ -1275,8 +1275,7 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But for nodes
           // close to the principal variation the cap is at (newDepth + 1), which will
           // allow these nodes to be searched deeper than the pv (up to 4 plies deeper).
-          Depth d = std::clamp(newDepth - r, 1, newDepth + ((ss+1)->distanceFromPv < depth));
-          dbg_mean_of(depth);
+          Depth d = std::clamp(newDepth - r, 1, newDepth + ((ss+1)->distanceFromPv < std::max(0, 6 - depth)));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
