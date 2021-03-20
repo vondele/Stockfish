8d8c9b7344d146ac1689f5658d9624ecfca93677
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-03-20 21:24:25 +0100

    Take 2. Bench: 4590566

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -202,7 +202,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, 1283 - 3075 * depth);
+          partial_insertion_sort(cur, endMoves, 1094 - 2772 * depth);
       }
 
       ++stage;
