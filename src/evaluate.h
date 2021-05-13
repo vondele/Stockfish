d2a9ca11b82b84e8c2fa8470f242c159a9ff6793
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-05-13 12:45:53 +0200

    Test net. Exp=run301 (depth=d7) Run=run_0 nn-epoch299.nnue nn-187d39f12eda.nnue. Bench: 3412966

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-82a4dd55cc6d.nnue"
+  #define EvalFileDefaultName   "nn-187d39f12eda.nnue"
 
   namespace NNUE {
 
