eed58dddfa2b81d4ff53082e7bae4ccc67d2c4f1
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-05-25 19:47:42 +0200

    Test net. run353/nnue-pytorch/exp/run_19/nn-epoch319.nnue nn-d5b18c03ba47.nnue. Bench: 3640829

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-13701a638508.nnue"
+  #define EvalFileDefaultName   "nn-d5b18c03ba47.nnue"
 
   namespace NNUE {
 
