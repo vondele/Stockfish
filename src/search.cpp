e17e6d1920fb769edd14a2eb5de60337d33d12f4
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-06-02 17:37:05 +0200

    Move king in check. Bench: 4500528

--- src/search.cpp
+++ src/search.cpp
@@ -1166,6 +1166,9 @@ moves_loop: // When in check, search starts from here
               if (ttCapture)
                   r++;
 
+              if (ss->inCheck && type_of(movedPiece) == KING)
+                  r++;
+
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
