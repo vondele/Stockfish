cc71105a5638c4ef7371c0d6fbc770553c709f12
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-06-02 17:17:54 +0200

    movepick, gives check. Bench: 4332377

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -106,7 +106,7 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
-          m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
+          m.value =  (depth > 6 && pos.gives_check(m) ? 2 : 1) * (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
