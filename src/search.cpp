7df298bb28919f5ea702442e95a9b3d7de4868cd
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-07-14 19:26:04 +0200

    Adjust bonus with check. Take 2. Bench: 5159038

--- src/search.cpp
+++ src/search.cpp
@@ -651,7 +651,7 @@ namespace {
             {
                 // Bonus for a quiet ttMove that fails high
                 if (!pos.capture_or_promotion(ttMove))
-                    update_quiet_stats(pos, ss, ttMove, stat_bonus(depth - ss->inCheck), depth);
+                    update_quiet_stats(pos, ss, ttMove, stat_bonus(depth + ss->inCheck), depth);
 
                 // Extra penalty for early quiet moves of the previous ply
                 if ((ss-1)->moveCount <= 2 && !priorCapture)
