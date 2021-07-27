48c15c585994d0d737e57d46b64c18cc7d48e0d5
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-07-27 22:15:52 +0200

    Increment by two in ID. Bench: 2060925

--- src/search.cpp
+++ src/search.cpp
@@ -317,7 +317,7 @@ void Thread::search() {
   int searchAgainCounter = 0;
 
   // Iterative deepening loop until requested to stop or the target depth is reached
-  while (   ++rootDepth < MAX_PLY
+  while (   (rootDepth = rootDepth + 2) < MAX_PLY
          && !Threads.stop
          && !(Limits.depth && mainThread && rootDepth > Limits.depth))
   {
