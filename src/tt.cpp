e33d5aba17a9e3c92b72678f84372ba600915868
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-08-05 18:51:47 +0200

    Tune replacement strat. Take 1. Bench: 5101949

--- src/tt.cpp
+++ src/tt.cpp
@@ -42,7 +42,7 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
   // Overwrite less valuable entries (cheapest checks first)
   if (b == BOUND_EXACT
       || (uint16_t)k != key16
-      || d - DEPTH_OFFSET > depth8 - 4)
+      || d - DEPTH_OFFSET > depth8 - 3)
   {
       assert(d > DEPTH_OFFSET);
       assert(d < 256 + DEPTH_OFFSET);
