6933be73d9c3d44a5210bc64cba0ff1d0c618a4d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-08-06 09:42:40 +0200

    Take 2. Bench 5318056

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -170,7 +170,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return (pos.rule50_count() > 20 && cur == moves) || pos.see_ge(*cur, Value(-69 * cur->value / 1024)) ?
+                       return (pos.rule50_count() > 50 && cur == moves) || pos.see_ge(*cur, Value(-69 * cur->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
