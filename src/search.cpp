bcd2dc5c65da71cce4637653b3d6c065645bdcc1
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-09-11 15:57:27 +0200

    Take 4. Bench: 5947695

--- src/search.cpp
+++ src/search.cpp
@@ -415,7 +415,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 4 + 6;
+              delta += delta / 4 + 7;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
