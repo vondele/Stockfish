ca9ff6c2faab180ddebccd6e353c389576c2a412
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-09-22 08:13:27 +0200

    Take 5. Bench: 6025988

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -106,8 +106,8 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
-          m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + 3 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+          m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
+                   + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
