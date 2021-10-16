dd0233fc51ae4fd7de909a542807ab8566e28f3a
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-10-16 20:22:52 +0200

    Take 2. Bench: 4719799

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -104,7 +104,7 @@ void MovePicker::score() {
       if constexpr (Type == CAPTURES)
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
-                   + 2000 * pos.gives_check(m);
+                   + 1000 * pos.gives_check(m);
 
       else if constexpr (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
