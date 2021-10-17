234225305463b9e64ca300c9847d10a07f3788c0
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-10-17 18:17:46 +0200

    Take 4. Bench: 5413063

--- src/search.cpp
+++ src/search.cpp
@@ -1507,7 +1507,7 @@ moves_loop: // When in check, search starts here
         if (PvNode && bestValue > alpha)
             alpha = bestValue;
 
-        futilityBase = bestValue + 170;
+        futilityBase = bestValue + 140;
     }
 
     const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory,
