49a6d02d6734361ab0a92d2a12b2274e53137b84
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-10-20 22:15:22 +0200

    Take 2. Bench: 5518272

--- src/search.cpp
+++ src/search.cpp
@@ -813,7 +813,7 @@ namespace {
 
     // Step 7. Futility pruning: child node (~50 Elo).
     // The depth condition is important for mate finding.
-    if (    depth < 9 - 5 * PvNode
+    if (    depth < 9 - 6 * PvNode
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
