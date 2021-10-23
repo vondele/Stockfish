3a85ae83798e35a6e6315790ec5d12921e66e26d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-10-23 14:32:03 +0200

    Tweak TT write criterion. Take 2. Bench: 6878160

--- src/tt.cpp
+++ src/tt.cpp
@@ -40,9 +40,9 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
       move16 = (uint16_t)m;
 
   // Overwrite less valuable entries (cheapest checks first)
-  if ((b == BOUND_EXACT && d - DEPTH_OFFSET > depth8 - 8)
+  if (b == BOUND_EXACT
       || (uint16_t)k != key16
-      || d - DEPTH_OFFSET > depth8 - 4)
+      || d - DEPTH_OFFSET > depth8 - 3)
   {
       assert(d > DEPTH_OFFSET);
       assert(d < 256 + DEPTH_OFFSET);
