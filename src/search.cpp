81ddf9a10c44c0415667f8f9b9610e1b9f1de5d7
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-11-05 16:56:19 +0100

    use tte depth for LMR. Bench: 7335873

--- src/search.cpp
+++ src/search.cpp
@@ -1203,6 +1203,9 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
 
+          if (ss->ttHit && newDepth - r < tte->depth() - 4)
+              r++;
+
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
