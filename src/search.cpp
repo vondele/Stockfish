f5c5c3bb1dacac7e9aaa834e162c5c4b44b2b350
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-11-05 17:10:39 +0100

    Take 2. Bench: 7411038

--- src/search.cpp
+++ src/search.cpp
@@ -1203,8 +1203,8 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
 
-          if (ss->ttHit && newDepth - r < tte->depth() - 4)
-              r++;
+          if (ss->ttHit && newDepth - r == tte->depth() - 4)
+              r--;
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
