5e380c621392582a8f4dbeb48bf58f8d0cd3dbff
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-11-08 22:30:39 +0100

    Take 1. Bench: 7246346

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1087,6 +1087,7 @@ Value Eval::evaluate(const Position& pos) {
   // but we switch to NNUE during long shuffling or with high material on the board.
 
   if (  !useNNUE
+      || pos.count<ALL_PIECES>() < 5
       || abs(eg_value(pos.psq_score())) * 5 > (850 + pos.non_pawn_material() / 64) * (5 + pos.rule50_count()))
       v = Evaluation<NO_TRACE>(pos).value();          // classical
   else
