6c557b8a2103cfbf218033afbc867ead4b9ab70c
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-11-10 18:07:51 +0100

    Take 2. Bench: 6686533

--- src/search.cpp
+++ src/search.cpp
@@ -898,7 +898,7 @@ namespace {
                 assert(pos.capture_or_promotion(move));
                 assert(depth >= 5);
 
-                if (++movecount > 3)
+                if (++movecount > depth)
                     break;
 
                 captureOrPromotion = true;
