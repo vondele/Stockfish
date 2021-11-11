8bbf445740dceb04734100dd7cf86caeeb6d8995
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-11-11 08:14:44 +0100

    Take 1. Bench: 6546087

--- src/search.cpp
+++ src/search.cpp
@@ -908,10 +908,10 @@ namespace {
                 pos.do_move(move, st);
 
                 // Perform a preliminary qsearch to verify that the move holds
-                value = -qsearch<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1);
+                value = -qsearch<NonPV>(pos, ss+1, -beta, -beta+1);
 
                 // If the qsearch held, perform the regular search
-                if (value >= probCutBeta)
+                if (value >= beta)
                     value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 4, !cutNode);
 
                 pos.undo_move(move);
