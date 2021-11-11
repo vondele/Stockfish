9df9f3348f06039455bfd47dc5d8b564b608f4da
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-11-11 08:22:43 +0100

    Remove qs. Bench: 7040985

--- src/search.cpp
+++ src/search.cpp
@@ -907,12 +907,7 @@ namespace {
 
                 pos.do_move(move, st);
 
-                // Perform a preliminary qsearch to verify that the move holds
-                value = -qsearch<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1);
-
-                // If the qsearch held, perform the regular search
-                if (value >= probCutBeta)
-                    value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 4, !cutNode);
+                value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 4, !cutNode);
 
                 pos.undo_move(move);
 
