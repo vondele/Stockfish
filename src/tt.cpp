ab27405cc30b063af25eaafba1e7576b70568c0c
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-11-17 11:29:35 +0100

    Take 1. Bench: 7033288

--- src/tt.cpp
+++ src/tt.cpp
@@ -50,7 +50,10 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
       key16     = (uint16_t)k;
       depth8    = (uint8_t)(d - DEPTH_OFFSET);
       genBound8 = (uint8_t)(TT.generation8 | uint8_t(pv) << 2 | b);
-      value16   = (int16_t)v;
+      if (std::abs(v) < VALUE_KNOWN_WIN)
+          value16   = (int16_t)(v * (256 - d) / 256);
+      else
+          value16   = (int16_t)v;
       eval16    = (int16_t)ev;
   }
 }
