c778e987adc7dccb5bfd2c2b66248506ba7fd9da
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-12-03 22:18:46 +0100

    Try a cubic term. Bench: 5200752

--- src/search.cpp
+++ src/search.cpp
@@ -1212,10 +1212,11 @@ moves_loop: // When in check, search starts here
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
-                         - 4923;
+                         - 4438;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / 14721;
+          int t = ss->statScore / 15891;
+          r -= t * (32 + 9 * t * t) / 32;
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
