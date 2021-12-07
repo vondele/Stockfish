a698cd3669d9a955a72a0d0b9eca76f971280f1d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-12-07 23:06:44 +0100

    Take 2. Bench: 4508716

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1091,8 +1091,7 @@ Value Eval::evaluate(const Position& pos) {
       v = Evaluation<NO_TRACE>(pos).value();          // classical
   else
   {
-       int scale = 1136
-                   + 20 * pos.non_pawn_material() / 1024;
+       int scale = 1360;
 
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
        Color stm      = pos.side_to_move();
