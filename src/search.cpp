65f74636bdf2025dce5f3f13797aac8683396db9
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-12-08 08:09:45 +0100

    Take 1. Bench: 4217250

--- src/search.cpp
+++ src/search.cpp
@@ -1239,7 +1239,7 @@ moves_loop: // When in check, search starts here
 
           // If the son is reduced and fails high it will be re-searched at full depth
           doFullDepthSearch = value > alpha && d < newDepth;
-          doDeeperSearch = value > alpha + 88;
+          doDeeperSearch = value > alpha + 88 && d > newDepth - 4;
           didLMR = true;
       }
       else
