f4115a5f236bbeb3f64b451f65282633adf7efab
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-12-26 08:09:59 +0100

    Tweak. Bench: 4487157

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((6 * d + 229) * d - 215 , 2000);
+    return std::min((4 * d + 245) * d - 121 , 2000);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
