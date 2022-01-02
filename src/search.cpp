d4f19d1ae5fe9cacee70668976f5d88ac6dddd66
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-01-02 07:17:39 +0100

    Tweak, param 140. Bench: 3900292

--- src/search.cpp
+++ src/search.cpp
@@ -1492,7 +1492,7 @@ moves_loop: // When in check, search starts here
         if (PvNode && bestValue > alpha)
             alpha = bestValue;
 
-        futilityBase = bestValue + 150;
+        futilityBase = bestValue + 140;
     }
 
     const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory,
