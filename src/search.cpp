3953990ceed0a34428e4d9e6ffd98b44456afd95
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-01-02 08:01:54 +0100

    Depth dependence for movecount pruning in qsearch. Take 2. Bench: 3925938

--- src/search.cpp
+++ src/search.cpp
@@ -1531,7 +1531,7 @@ moves_loop: // When in check, search starts here
           &&  type_of(move) != PROMOTION)
       {
 
-          if (moveCount > std::max(1, 3 + depth))
+          if (moveCount > std::max(2, 3 + depth))
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
