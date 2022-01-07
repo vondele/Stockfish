2283d31fa38b660a216f4a9bca574e59219b07d3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-01-07 14:59:56 +0100

    Take 3. Bench: 4198486

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1101,9 +1101,9 @@ Value Eval::evaluate(const Position& pos) {
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
-       int complexity = 35 * abs(nnue - psq) / 256;
+       int complexity = 44 * abs(nnue - psq) / 256;
 
-       optimism = optimism * (44 + complexity) / 32;
+       optimism = optimism * (32 + complexity) / 32;
        v = (nnue + optimism) * scale / 1024 - optimism;
 
        if (pos.is_chess960())
