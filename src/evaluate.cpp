5f624a31669e9ad5eaac3a2097f6cb922e3063d4
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-01-07 23:24:23 +0100

    Take 5. Bench: 4048312

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1069,8 +1069,8 @@ make_v:
         && pos.piece_on(SQ_G7) == B_PAWN)
         correction += CorneredBishop;
 
-    return pos.side_to_move() == WHITE ?  Value(7 * correction)
-                                       : -Value(7 * correction);
+    return pos.side_to_move() == WHITE ?  Value(2 * correction)
+                                       : -Value(2 * correction);
   }
 
 } // namespace Eval
