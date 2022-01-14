1146fcda378405a18b15ec435c845dbcbd16a473
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-01-14 16:26:37 +0100

    Based on sg idea. Increasebonus depth on complexity. Take 3. Bench: 5109353

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d, int complexity) {
-    d = d + complexity / 512;
+    d = d + complexity / 128;
     return std::min((6 * d + 229) * d - 215, 2000);
   }
 
