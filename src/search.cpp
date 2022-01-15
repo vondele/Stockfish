4f1000a634722ecdc2ed0cf4f9afc171dea8def6
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-01-15 10:18:45 +0100

    Take 5. Bench: 4864321

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d, int complexity) {
-    if (complexity > 128)
+    if (complexity > 256)
         d++;
     return std::min((6 * d + 229) * d - 215, 2000);
   }
