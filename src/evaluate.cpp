71588d7ffd004503c4b846ed47dba1e48f8afbc5
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-02-03 20:35:24 +0100

    Tune 3 params. Bench: 4311164

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -259,6 +259,9 @@ namespace {
 
   constexpr Value CorneredBishop = Value(50);
 
+  int param1 = 44, param2 = 114, param3 = 20;
+  TUNE(param1, param2, param3);
+
   // Assorted bonuses and penalties
   constexpr Score UncontestedOutpost  = S(  1, 10);
   constexpr Score BishopOnKingRing    = S( 24,  0);
@@ -1097,14 +1100,16 @@ Value Eval::evaluate(const Position& pos) {
   if (useNNUE && !useClassical)
   {
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
-       int scale      = 1136 + 20 * pos.non_pawn_material() / 1024;
+
        Color stm      = pos.side_to_move();
-       Value optimism = pos.this_thread()->optimism[stm];
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
        int complexity = 35 * abs(nnue - psq) / 256;
 
-       optimism = optimism * (44 + complexity) / 32;
-       v = (nnue + optimism) * scale / 1024 - optimism;
+       Value optimism = pos.this_thread()->optimism[stm];
+       optimism = optimism * (param1 + complexity) * (param2 * 1024 + param3 * pos.non_pawn_material()) / (32 * 1024 * 1024);
+
+       int scale      = 1136 + 20 * pos.non_pawn_material() / 1024;
+       v = nnue * scale / 1024 + optimism;
 
        if (pos.is_chess960())
            v += fix_FRC(pos);
