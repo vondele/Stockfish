68244e8a1da916f550e64529f917a96017861038
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-02-09 18:02:10 +0100

    Take 15. Bench: 4876959

--- src/search.cpp
+++ src/search.cpp
@@ -920,7 +920,7 @@ namespace {
 moves_loop: // When in check, search starts here
 
     // Step 12. A small Probcut idea, when we are in check (~0 Elo)
-    probCutBeta = beta + 401;
+    probCutBeta = beta + 409;
     if (   ss->inCheck
         && !PvNode
         && depth >= 4
