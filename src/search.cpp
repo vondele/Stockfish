2ebd80c63588a59c464306441840fb0cfe62ffb6
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-02-09 17:47:10 +0100

    Take 3. Bench: 4683821

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((7 * d + 254) * d - 206 , 1990);
+    return std::min((6 * d + 229) * d - 215 , 2000);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
