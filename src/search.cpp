02dd72e04cebf8c996ac25f25bfe550b8dc60224
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-05-03 20:29:13 +0200

    Take 1. Bench: 6871028

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
-              r -= 1 + 15 / ( 3 + depth );
+              r -= 1 + 15 / ( 2 + depth );
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
