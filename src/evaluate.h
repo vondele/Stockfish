912e5c9bb85003574f4895378e5cf2e0ca673066
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-06-21 06:35:30 +0200

    Test run1021/easy_train_data/experiments/experiment_13/training/run_0/nn-epoch719.nnue localElo: 1.1  +-  3.2 Name: nn-35ff887286d7.nnue Bench: 5304928

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-59bb200942fa.nnue"
+  #define EvalFileDefaultName   "nn-35ff887286d7.nnue"
 
   namespace NNUE {
 
