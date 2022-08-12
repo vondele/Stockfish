63686f75187d0f4317662709cbccae5948e26874
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-08-12 11:24:58 +0200

    Take 2. Bench: 6685403

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1084,7 +1084,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
        v = (nnue * scale + optimism * (scale - 754)) / 1024;
 
        Color rootColor = pos.this_thread()->rootColor;
-       v+= (rootColor == stm ? -1 : 1 ) * pos.count<QUEEN>() * 8;
+       v+= (rootColor == stm ? -1 : 1 ) * pos.count<QUEEN>() * 16;
   }
 
   // Damp down the evaluation linearly when shuffling
