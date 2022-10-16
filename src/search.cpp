b51a0ca15d6a88a61debe8a62119ce78baa060d9
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-10-16 18:05:17 +0200

    extensions for captures in rook endgames. Bench: 4497044

--- src/search.cpp
+++ src/search.cpp
@@ -1104,6 +1104,9 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5177)
               extension = 1;
+          else if (   priorCapture
+                   && (pos.non_pawn_material() == 2 * RookValueMg || pos.non_pawn_material() == 4 * RookValueMg))
+              extension = 1;
       }
 
       // Add extension to new depth
