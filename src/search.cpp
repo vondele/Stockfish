45401a35d1c98dc97d47791a104bd073ea9997f4
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-10-28 22:01:57 +0200

    Disable probCut for r50c >= N. Take 2. Bench: 4289748

--- src/search.cpp
+++ src/search.cpp
@@ -848,7 +848,7 @@ namespace {
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
         &&  depth > 4
-        &&  pos.rule50_count() < 16
+        &&  pos.rule50_count() < 32
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
         // there and in further interactions with transposition table cutoff depth is set to depth - 3
