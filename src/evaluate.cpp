e08fef9dc0835e7a6a4cc604b1f93d8fee18cc3a
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-11-23 00:04:30 +0100

    Scale with game ply. Take 1. bench: 4267603

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1087,6 +1087,8 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   // Damp down the evaluation linearly when shuffling
   v = v * (195 - pos.rule50_count()) / 211;
 
+  v = v * 1024 / (1024 + pos.game_ply());
+
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
