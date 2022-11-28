b13756a246997ac587f24cf2c24186bf6b25ceda
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-11-28 21:36:29 +0100

    Adjust by PV length

--- src/thread.cpp
+++ src/thread.cpp
@@ -222,7 +222,7 @@ Thread* ThreadPool::get_best_thread() const {
 
     // Vote according to score and depth, and select the best thread
     auto thread_value = [minScore](Thread* th) {
-            return (th->rootMoves[0].score - minScore + 14) * int(th->completedDepth + 1 - th->rootMoves[0].scoreUpperbound);
+            return (th->rootMoves[0].score - minScore + 14) * int(th->rootMoves[0].pv.size());
         };
 
     for (Thread* th : *this)
