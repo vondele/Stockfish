7c1d191614b3f92f00407292865ff1d340de2ec8
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-11-28 21:31:14 +0100

    Adjust by scoreUpperbound

--- src/thread.cpp
+++ src/thread.cpp
@@ -222,7 +222,7 @@ Thread* ThreadPool::get_best_thread() const {
 
     // Vote according to score and depth, and select the best thread
     auto thread_value = [minScore](Thread* th) {
-            return (th->rootMoves[0].score - minScore + 14) * int(th->completedDepth);
+            return (th->rootMoves[0].score - minScore + 14) * int(th->completedDepth + 1 - th->rootMoves[0].scoreUpperbound);
         };
 
     for (Thread* th : *this)
