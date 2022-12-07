e89d2355afffe8343e12f5cfd41f1ac6456dc091
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2022-12-07 02:49:09 +0100

    Simplify Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -9,18 +9,9 @@ LOCAL_CPP_FEATURES :=
 
 ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
   LOCAL_CFLAGS += -DIS_64BIT -DUSE_NEON=8 -DUSE_POPCNT
-else
-  ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-    LOCAL_CFLAGS += -DUSE_NEON=7 -DUSE_POPCNT
-  else
-    ifeq ($(TARGET_ARCH_ABI), x86)
-      LOCAL_CFLAGS += -mssse3 -mfpmath=sse -m32 -DUSE_SSSE3 -DUSE_SSE2 -DUSE_MMX
-    else
-      ifeq ($(TARGET_ARCH_ABI), x86_64)
-        LOCAL_CFLAGS += -msse4.2 -mpopcnt -m64 -DIS_64BIT -DUSE_SSE41 -DUSE_SSSE3 -DUSE_SSE2 -DUSE_MMX -DUSE_POPCNT
-      endif
-    endif
-  endif
+endif
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+  LOCAL_CFLAGS += -DUSE_NEON=7 -DUSE_POPCNT
 endif
 
 LOCAL_LDFLAGS += 
