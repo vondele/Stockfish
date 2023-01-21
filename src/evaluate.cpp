dd24d21596c2af10968fad7ff5ff9d97911f6b49
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2023-01-21 19:34:49 +0100

    Take 5. Bench: 4040506

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1087,7 +1087,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   // Damp down the evaluation linearly when shuffling
   v = v * (200 - pos.rule50_count()) / 214;
 
-  v = v * (64 + std::min(32, pos.this_thread()->seCount)) / 64;
+  v = v * (128 + std::min(64, pos.this_thread()->seCount)) / 128;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
