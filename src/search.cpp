6dfccffeae7818c2a2df5d1f48eec3feac064d34
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2023-02-20 20:11:02 +0100

    Take 4. Bench: 3726250

--- src/search.cpp
+++ src/search.cpp
@@ -950,7 +950,7 @@ moves_loop: // When in check, search starts here
                          && (tte->bound() & BOUND_UPPER)
                          && tte->depth() >= depth;
 
-    if (Eval::useNNUE && likelyFailLow && !ss->inCheck && depth > 4)
+    if (Eval::useNNUE && likelyFailLow && !ss->inCheck)
         Eval::NNUE::hint_common_parent_position(pos);
 
 
