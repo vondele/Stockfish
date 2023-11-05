e3c8c2a40835a7a1aa12314532e3513fbf8e5cc9
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2023-11-05 12:14:09 +0100

    Take 3. Bench: 1293496

--- src/search.cpp
+++ src/search.cpp
@@ -369,8 +369,8 @@ void Thread::search() {
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
             // Adjust optimism based on root move's averageScore (~4 Elo)
-            optimism[us]  = 103 * (avg + 33) / (std::abs(avg + 34) + 119) - 15;
-            optimism[~us] = -116 * (avg + 40) / (std::abs(avg + 12) + 123) + 15;
+            optimism[us]  = 103 * (avg + 33) / (std::abs(avg + 34) + 119) - 20;
+            optimism[~us] = -116 * (avg + 40) / (std::abs(avg + 12) + 123) + 20;
 
             // Start with a small aspiration window and, in the case of a fail
             // high/low, re-search with a bigger window until we don't fail
