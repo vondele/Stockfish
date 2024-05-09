e9e267af745284ec4231db3042c3a0f96f60bd82
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2024-05-09 10:43:28 +0200

    Rebased testing of https://tests.stockfishchess.org/tests/view/663c6172c0b75d7f7b97f235 Bench: 2360221

--- src/search.cpp
+++ src/search.cpp
@@ -821,7 +821,8 @@ Value Search::Worker::search(
     // Step 10. Internal iterative reductions (~9 Elo)
     // For PV nodes without a ttMove, we decrease depth by 3.
     if (PvNode && !ttMove)
-        depth -= 3;
+        depth -= 4 - (thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] < -350)
+                   + (thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] > 350);
 
     // Use qsearch if depth <= 0.
     if (depth <= 0)
