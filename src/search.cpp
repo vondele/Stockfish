dbaa245e12e29d898773a86a10bf5739fc102edf
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2024-06-13 20:59:48 +0200

    Break it.
    Bench: 1173342

--- src/search.cpp
+++ src/search.cpp
@@ -1484,8 +1484,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
               to_corrected_static_eval(unadjustedStaticEval, *thisThread, pos);
 
             // ttValue can be used as a better position evaluation (~13 Elo)
-            if (std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY
-                && (ttData.bound & (ttData.value > bestValue ? BOUND_LOWER : BOUND_UPPER)))
+            if (true && (ttData.bound & (ttData.value > bestValue ? BOUND_LOWER : BOUND_UPPER)))
                 bestValue = ttData.value;
         }
         else
