dc3280a8f580a9e466d2c75888eeda5a93a8e693
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2024-09-19 21:35:23 +0200

    Next

--- src/search.cpp
+++ src/search.cpp
@@ -800,7 +800,7 @@ Value Search::Worker::search(
     if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
         && ss->staticEval >= beta - 23 * depth + 400 && !excludedMove && pos.non_pawn_material(us)
         && ss->ply >= thisThread->nmpMinPly && beta > VALUE_TB_LOSS_IN_MAX_PLY
-        && (depth < 20 || countLegal() > depth / 4 + 5))
+        && (depth < 18 || 14 * countLegal() > 4 * depth + 53))
     {
         assert(eval - beta >= 0);
 
