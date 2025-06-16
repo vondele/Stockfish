7f4071ea843c593dfe90526b5585afb9cc3b6543
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-06-17 00:08:27 +0200

    Take 2. Bench: 2102421

--- src/evaluate.h
+++ src/evaluate.h
@@ -34,7 +34,7 @@ namespace Eval {
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
 #define EvalFileDefaultNameBig "nn-1c0000000000.nnue"
-#define EvalFileDefaultNameSmall "nn-e89a9f2dd499.nnue"
+#define EvalFileDefaultNameSmall "nn-1da13dfe18ce.nnue"
 
 namespace NNUE {
 struct Networks;
