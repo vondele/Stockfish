a3ca2ef1a6dc1490f5a652ee7debfc4fb004c19e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-09-13 18:22:44 +0200

    Train both nets from scratch. Bench: 2418404

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,8 +33,8 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-1c0000000000.nnue"
-#define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
+#define EvalFileDefaultNameBig "nn-bb12304cb7ba.nnue"
+#define EvalFileDefaultNameSmall "nn-da44bae4ff7f.nnue"
 
 namespace NNUE {
 struct Networks;
