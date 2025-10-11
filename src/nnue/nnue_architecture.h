cf84ec7d9f91891f08dd650cbffd5ac4f585eaf4
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-10-11 22:25:58 +0200

    L1=768

--- src/nnue/nnue_architecture.h
+++ src/nnue/nnue_architecture.h
@@ -38,7 +38,7 @@ namespace Stockfish::Eval::NNUE {
 using FeatureSet = Features::HalfKAv2_hm;
 
 // Number of input feature dimensions after conversion
-constexpr IndexType TransformedFeatureDimensionsBig = 512;
+constexpr IndexType TransformedFeatureDimensionsBig = 768;
 constexpr int       L2Big                           = 15;
 constexpr int       L3Big                           = 32;
 
