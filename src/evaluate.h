5b527eda3b72046fa3252df09c85c5160ffc6475
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2026-08-02 21:34:52 +0200

    Take 3. Bench: 2272360

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-14fcdd00256b.nnue"
+#define EvalFileDefaultName "nn-439d90fbcc36.nnue"
 
 namespace NNUE {
 class Network;
