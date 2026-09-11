c6327ffeae171a4065b8ce178ec31f0db894bed3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2026-09-11 21:25:00 +0200

    Take 2. Rebased.  Bench: 1660220

--- src/search.cpp
+++ src/search.cpp
@@ -1381,7 +1381,7 @@ moves_loop:  // When in check, search starts here
             // beyond the first move depth.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d = std::max(1, newDepth + std::min(-r / 1024, ss->ply < rootDepth ? 2 : 0)) + PvNode;
+            Depth d = std::max(1, newDepth + std::min(-r / 1024, ss->ply < 2 * rootDepth ? 2 : 0)) + PvNode;
 
             ss->reduction = newDepth - d;
             value         = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
